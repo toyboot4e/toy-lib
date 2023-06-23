22a87d25d2946b0f9b50c07e4bb0ab96c5ccf621
toyboot4e <toyboot4e@gmail.com>
2023-06-23 23:41:27 +0900

    use manual unboxing for `ModInt p`

--- src/Data/ModInt.hs
+++ src/Data/ModInt.hs
@@ -8,17 +8,20 @@
 {-# LANGUAGE TypeFamilies #-}
 
 -- | `Int` with automatic moudlo arithmetic performed. Depends on @Math.PowMod@.
-
 module Data.ModInt where
 
+import Control.Monad
+import Data.Coerce
 import Data.Proxy
 import qualified Data.Ratio as Ratio
 import Data.Semigroup
 import Data.SemigroupAction
+import Data.Vector.Generic as VG
+import Data.Vector.Generic.Mutable as VGM
+import Data.Vector.Unboxed as VU
 import Data.Vector.Unboxed.Deriving (derivingUnbox)
+import Data.Vector.Unboxed.Mutable as VUM
 import Math.PowMod (invModF)
-
--- {{{ ModInt
 
 -- | Type level constant `Int` value.
 -- TODO: Replace with `GHC.TypaNats.KnownNat`: <https://zenn.dev/mod_poppo/books/haskell-type-level-programming/viewer/ghc-typenats>
@@ -28,12 +31,6 @@ class TypeInt a where
 -- | `Int` with automatic moudlo arithmetic performed.
 newtype ModInt p = ModInt {toInt :: Int}
   deriving (Eq)
-
-derivingUnbox
-  "ModInt"
-  [t|forall p. ModInt p -> Int|]
-  [|\(ModInt !x) -> x|]
-  [|\ !x -> ModInt x|]
 
 instance Show (ModInt p) where
   show = show . toInt
@@ -57,4 +54,57 @@ instance TypeInt p => Fractional (ModInt p) where
 instance TypeInt p => SemigroupAction (Product (ModInt p)) (ModInt p) where
   sact (Product !x1) !x2 = x1 * x2
 
--- }}}
+-- derivingUnbox
+--   "ModInt"
+--   [t|forall p. ModInt p -> Int|]
+--   [|\(ModInt !x) -> x|]
+--   [|\ !x -> ModInt x|]
+
+newtype instance VUM.MVector s (ModInt p) = MV_ModInt (VUM.MVector s Int)
+
+newtype instance VU.Vector (ModInt p) = V_ModInt (VU.Vector Int)
+
+instance VU.Unbox (ModInt p)
+
+instance VGM.MVector VUM.MVector (ModInt p) where
+  basicLength (MV_ModInt v) = VGM.basicLength v
+  {-# INLINE basicLength #-}
+  basicUnsafeSlice i n (MV_ModInt v) = MV_ModInt $ VGM.basicUnsafeSlice i n v
+  {-# INLINE basicUnsafeSlice #-}
+  basicOverlaps (MV_ModInt v1) (MV_ModInt v2) = VGM.basicOverlaps v1 v2
+  {-# INLINE basicOverlaps #-}
+  basicUnsafeNew n = MV_ModInt <$> VGM.basicUnsafeNew n
+  {-# INLINE basicUnsafeNew #-}
+  basicInitialize (MV_ModInt v) = VGM.basicInitialize v
+  {-# INLINE basicInitialize #-}
+  basicUnsafeReplicate n x = MV_ModInt <$> VGM.basicUnsafeReplicate n (coerce x)
+  {-# INLINE basicUnsafeReplicate #-}
+  basicUnsafeRead (MV_ModInt v) i = coerce <$> VGM.basicUnsafeRead v i
+  {-# INLINE basicUnsafeRead #-}
+  basicUnsafeWrite (MV_ModInt v) i x = VGM.basicUnsafeWrite v i (coerce x)
+  {-# INLINE basicUnsafeWrite #-}
+  basicClear (MV_ModInt v) = VGM.basicClear v
+  {-# INLINE basicClear #-}
+  basicSet (MV_ModInt v) x = VGM.basicSet v (coerce x)
+  {-# INLINE basicSet #-}
+  basicUnsafeCopy (MV_ModInt v1) (MV_ModInt v2) = VGM.basicUnsafeCopy v1 v2
+  {-# INLINE basicUnsafeCopy #-}
+  basicUnsafeMove (MV_ModInt v1) (MV_ModInt v2) = VGM.basicUnsafeMove v1 v2
+  {-# INLINE basicUnsafeMove #-}
+  basicUnsafeGrow (MV_ModInt v) n = MV_ModInt <$> VGM.basicUnsafeGrow v n
+  {-# INLINE basicUnsafeGrow #-}
+
+instance VG.Vector VU.Vector (ModInt p) where
+  basicUnsafeFreeze (MV_ModInt v) = V_ModInt <$> VG.basicUnsafeFreeze v
+  {-# INLINE basicUnsafeFreeze #-}
+  basicUnsafeThaw (V_ModInt v) = MV_ModInt <$> VG.basicUnsafeThaw v
+  {-# INLINE basicUnsafeThaw #-}
+  basicLength (V_ModInt v) = VG.basicLength v
+  {-# INLINE basicLength #-}
+  basicUnsafeSlice i n (V_ModInt v) = V_ModInt $ VG.basicUnsafeSlice i n v
+  {-# INLINE basicUnsafeSlice #-}
+  basicUnsafeIndexM (V_ModInt v) i = coerce <$> VG.basicUnsafeIndexM v i
+  {-# INLINE basicUnsafeIndexM #-}
+  basicUnsafeCopy (MV_ModInt mv) (V_ModInt v) = VG.basicUnsafeCopy mv v
+  elemseq _ = seq
+  {-# INLINE elemseq #-}
