27e29adc1017c3ed4fddf72cf7e8ab29ce402345
toyboot4e <toyboot4e@gmail.com>
2023-06-28 20:20:06 +0900

    add `View3d` module (untested)

--- src/Data/Vector/View2d.hs
+++ src/Data/Vector/View2d.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 
-module Data.Vector.View where
+module Data.Vector.View2d where
 
 import Control.Monad.Primitive
 import qualified Data.Vector as V
@@ -10,52 +10,57 @@ import qualified Data.Vector.Mutable as VM
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as VUM
 
+type Index2d = (Int, Int)
+
+-- | @View2d (originalVector)
+type View2d = V.Vector
+
 -- | Creates a 2D view to an immutable vector.
 {-# INLINE view2dVG #-}
-view2dVG :: (VG.Vector v a) => (Int, Int) -> v a -> V.Vector (v a)
-view2dVG (!h, !w) !view = V.generate h (\i -> VG.slice (w * i) w view)
+view2dVG :: (VG.Vector v a) => Index2d -> v a -> View2d (v a)
+view2dVG (!h, !w) !view = V.generate h (\ih -> VG.slice (ih * w) w view)
 
 {-# INLINE withView2dVG #-}
-withView2dVG :: (VG.Vector v a) => (Int, Int) -> v a -> (v a, V.Vector (v a))
+withView2dVG :: (VG.Vector v a) => Index2d -> v a -> (v a, View2d (v a))
 withView2dVG !hw !view = (view, view2dVG hw view)
 
 -- | Creates a 2D view to a mutable vector.
 {-# INLINE view2dVGM #-}
-view2dVGM :: (VGM.MVector v a) => (Int, Int) -> v s a -> V.Vector (v s a)
-view2dVGM (!h, !w) !view = V.generate h (\i -> VGM.slice (w * i) w view)
+view2dVGM :: (VGM.MVector v a) => Index2d -> v s a -> View2d (v s a)
+view2dVGM (!h, !w) !view = V.generate h (\ih -> VGM.slice (ih * w) w view)
 
 {-# INLINE withView2dVGM #-}
-withView2dVGM :: (VGM.MVector v a) => (Int, Int) -> v s a -> (v s a, V.Vector (v s a))
+withView2dVGM :: (VGM.MVector v a) => Index2d -> v s a -> (v s a, View2d (v s a))
 withView2dVGM !hw !view = (view, view2dVGM hw view)
 
 -- | Creates a 2D mutable vector.
 {-# INLINE new2dVGM #-}
-new2dVGM :: (PrimMonad m, VGM.MVector v a) => (Int, Int) -> a -> m (v (PrimState m) a, V.Vector (v (PrimState m) a))
+new2dVGM :: (PrimMonad m, VGM.MVector v a) => Index2d -> a -> m (v (PrimState m) a, View2d (v (PrimState m) a))
 new2dVGM (!h, !w) !e0 = withView2dVGM (h, w) <$> VGM.replicate (h * w) e0
 
 -- | Creates a 2D unboxed mutable vector.
 {-# INLINE new2dVUM #-}
-new2dVUM :: (PrimMonad m, VU.Unbox a) => (Int, Int) -> a -> m (VUM.MVector (PrimState m) a, V.Vector (VUM.MVector (PrimState m) a))
+new2dVUM :: (PrimMonad m, VU.Unbox a) => Index2d -> a -> m (VUM.MVector (PrimState m) a, View2d (VUM.MVector (PrimState m) a))
 new2dVUM = new2dVGM
 
 -- | Creates a 2D boxed mutable vector.
 {-# INLINE new2dVM #-}
-new2dVM :: (PrimMonad m) => (Int, Int) -> a -> m (VM.MVector (PrimState m) a, V.Vector (VM.MVector (PrimState m) a))
+new2dVM :: (PrimMonad m) => Index2d -> a -> m (VM.MVector (PrimState m) a, View2d (VM.MVector (PrimState m) a))
 new2dVM = new2dVGM
 
 -- | 2D immutable vector indexing.
 {-# INLINE idx2d #-}
-idx2d :: (VG.Vector v a) => V.Vector (v a) -> (Int, Int) -> a
+idx2d :: (VG.Vector v a) => View2d (v a) -> Index2d -> a
 idx2d view (!y, !x) = view V.! y VG.! x
 
 {-# INLINE read2d #-}
-read2d :: (PrimMonad m, VGM.MVector v a) => V.Vector (v (PrimState m) a) -> (Int, Int) -> m a
+read2d :: (PrimMonad m, VGM.MVector v a) => View2d (v (PrimState m) a) -> Index2d -> m a
 read2d !view (!y, !x) = VGM.read (view V.! y) x
 
 {-# INLINE write2d #-}
-write2d :: (PrimMonad m, VGM.MVector v a) => V.Vector (v (PrimState m) a) -> (Int, Int) -> a -> m ()
+write2d :: (PrimMonad m, VGM.MVector v a) => View2d (v (PrimState m) a) -> Index2d -> a -> m ()
 write2d !view (!y, !x) = VGM.write (view V.! y) x
 
 {-# INLINE modify2d #-}
-modify2d :: (PrimMonad m, VGM.MVector v a) => V.Vector (v (PrimState m) a) -> (a -> a) -> (Int, Int) -> m ()
+modify2d :: (PrimMonad m, VGM.MVector v a) => View2d (v (PrimState m) a) -> (a -> a) -> Index2d -> m ()
 modify2d !view !alter (!y, !x) = VGM.modify (view V.! y) alter x
---
+++ src/Data/Vector/View3d.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Data.Vector.View3d where
+
+import Control.Monad.Primitive
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Generic.Mutable as VGM
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
+
+type Index3d = (Int, Int, Int)
+
+-- | @View3d (originalVector)
+type View3d a = V.Vector (V.Vector a)
+
+-- | Creates a 3D view to an immutable vector.
+{-# INLINE view3dVG #-}
+view3dVG :: (VG.Vector v a) => Index3d -> v a -> View3d (v a)
+view3dVG (!d, !h, !w) !view = V.generate d (\id_ -> V.generate h (\ih -> VG.slice (id_ * (h * w) + ih * w) w view))
+
+{-# INLINE withView3dVG #-}
+withView3dVG :: (VG.Vector v a) => Index3d -> v a -> (v a, View3d (v a))
+withView3dVG !hw !view = (view, view3dVG hw view)
+
+-- | Creates a 3D view to a mutable vector.
+{-# INLINE view3dVGM #-}
+view3dVGM :: (VGM.MVector v a) => Index3d -> v s a -> View3d (v s a)
+view3dVGM (!d, !h, !w) !view = V.generate d (\id_ -> V.generate h (\ih -> VGM.slice (id_ * (h * w) + ih * w) w view))
+
+{-# INLINE withView3dVGM #-}
+withView3dVGM :: (VGM.MVector v a) => Index3d -> v s a -> (v s a, View3d (v s a))
+withView3dVGM !hw !view = (view, view3dVGM hw view)
+
+-- | Creates a 3D mutable vector.
+{-# INLINE new3dVGM #-}
+new3dVGM :: (PrimMonad m, VGM.MVector v a) => Index3d -> a -> m (v (PrimState m) a, View3d (v (PrimState m) a))
+new3dVGM (!d, !h, !w) !e0 = withView3dVGM (d, h, w) <$> VGM.replicate (d * h * w) e0
+
+-- | Creates a 3D unboxed mutable vector.
+{-# INLINE new3dVUM #-}
+new3dVUM :: (PrimMonad m, VU.Unbox a) => Index3d -> a -> m (VUM.MVector (PrimState m) a, View3d (VUM.MVector (PrimState m) a))
+new3dVUM = new3dVGM
+
+-- | Creates a 3D boxed mutable vector.
+{-# INLINE new3dVM #-}
+new3dVM :: (PrimMonad m) => Index3d -> a -> m (VM.MVector (PrimState m) a, View3d (VM.MVector (PrimState m) a))
+new3dVM = new3dVGM
+
+-- | 3D immutable vector indexing.
+{-# INLINE idx3d #-}
+idx3d :: (VG.Vector v a) => View3d (v a) -> Index3d -> a
+idx3d view (!d, !y, !x) = view V.! d V.! y VG.! x
+
+{-# INLINE read3d #-}
+read3d :: (PrimMonad m, VGM.MVector v a) => View3d (v (PrimState m) a) -> Index3d -> m a
+read3d !view (!d, !y, !x) = VGM.read (view V.! d V.! y) x
+
+{-# INLINE write3d #-}
+write3d :: (PrimMonad m, VGM.MVector v a) => View3d (v (PrimState m) a) -> Index3d -> a -> m ()
+write3d !view (!d, !y, !x) = VGM.write (view V.! d V.! y) x
+
+{-# INLINE modify3d #-}
+modify3d :: (PrimMonad m, VGM.MVector v a) => View3d (v (PrimState m) a) -> (a -> a) -> Index3d -> m ()
+modify3d !view !alter (!d, !y, !x) = VGM.modify (view V.! d V.! y) alter x
