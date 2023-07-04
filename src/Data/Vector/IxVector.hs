05718486aeb653ddbaebf90a4aa67ae63b276d85
toyboot4e <toyboot4e@gmail.com>
2023-07-04 22:39:36 +0900

    update IxVector

--- src/Data/Vector/IxVector.hs
+++ src/Data/Vector/IxVector.hs
@@ -16,26 +16,26 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 import ToyLib.Macro (dbgAssert)
 
 -- | N-dimensional @Vector@ or @MVector@ with `Data.Ix`.
-data IxVector v i = IxVector {boundsIV :: !(i, i), vecIV :: !v}
+data IxVector i v = IxVector {boundsIV :: !(i, i), vecIV :: !v}
 
-(.!) :: (Ix i, VG.Vector v a) => IxVector (v a) i -> i -> a
+(.!) :: (Ix i, VG.Vector v a) => IxVector i (v a) -> i -> a
 (.!) IxVector {..} i = vecIV VG.! (index boundsIV i)
 
 -- | Reads a value from `IxVector`.
 {-# INLINE readIV #-}
-readIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector (v (PrimState m) a) i -> i -> m a
+readIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector i (v (PrimState m) a) -> i -> m a
 readIV IxVector {..} i = VGM.read vecIV (index boundsIV i)
 
 -- | Writes a value to `IxVector`.
 {-# INLINE writeIV #-}
-writeIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector (v (PrimState m) a) i -> i -> a -> m ()
+writeIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector i (v (PrimState m) a) -> i -> a -> m ()
 writeIV IxVector {..} i a = VGM.write vecIV (index boundsIV i) a
 
 {-# INLINE modifyIV #-}
-modifyIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector (v (PrimState m) a) i -> (a -> a) -> i -> m ()
+modifyIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector i (v (PrimState m) a) -> (a -> a) -> i -> m ()
 modifyIV IxVector {..} !alter i = VGM.modify vecIV alter (index boundsIV i)
 
 {-# INLINE swapIV #-}
-swapIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector (v (PrimState m) a) i -> i -> i -> m ()
+swapIV :: (Ix i, PrimMonad m, VGM.MVector v a) => IxVector i (v (PrimState m) a) -> i -> i -> m ()
 swapIV IxVector {..} !i1 !i2 = VGM.swap vecIV (index boundsIV i1) (index boundsIV i2)
 
