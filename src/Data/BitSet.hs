54893725490b0110e41f8f9c5917192b4527e6eb
toyboot4e <toyboot4e@gmail.com>
2023-08-04 23:05:19 +0900

    add `powersetVU` (not tested yet)

--- src/Data/BitSet.hs
+++ src/Data/BitSet.hs
@@ -8,6 +8,7 @@ import Control.Monad
 import Data.Bits
 import Data.List (unfoldr)
 import Data.Tuple.Extra (dupe)
+import qualified Data.Vector.Unboxed as VU
 
 -- | Originally by @yamate11
 powersetM_ :: (Bits a, Num a, Monad m) => a -> (a -> m ()) -> m ()
@@ -23,3 +24,13 @@ powerset !a = a : unfoldr f a
   where
     f 0 = Nothing
     f !x = Just . dupe $! a .&. (x - 1)
+
+-- | TODO: Working as expected?
+powersetVU :: (Bits a, Num a, VU.Unbox a) => a -> VU.Vector a
+powersetVU !a = VU.unfoldr f a
+  where
+    f (-1) = Nothing
+    f 0 = Just (0, -1)
+    f !x =
+      let !x' = a .&. (x - 1)
+       in Just (x, x')
