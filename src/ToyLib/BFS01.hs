69dae0bc37fb8f19ae45a08449074dab24997dfa
toyboot4e <toyboot4e@gmail.com>
2023-09-05 22:36:11 +0900

    01-BFS deserves a separate file

---
+++ src/ToyLib/BFS01.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE LambdaCase #-}
+
+-- | 01-BFS
+module ToyLib.BFS01 where
+
+import Control.Monad
+import Control.Monad.Fix
+import Data.Bool (bool)
+import Data.Buffer
+import Data.Ix
+import Data.Tuple.Extra (both)
+import Data.Vector.IxVector
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import ToyLib.Prelude (repM_)
+
+-- 01-BFS: zero cost with same direction.
+bfs01_grid4_typical043 :: IxUVector (Int, Int) Bool -> (Int, Int) -> IxUVector (Int, Int, Int) Int
+bfs01_grid4_typical043 !isBlock !start = IxVector boundsExt $ VU.create $ do
+  -- vec @! (dir, y, x)
+  !vec <- IxVector boundsExt <$> VUM.replicate (4 * nVerts) undef
+
+  let !redundantSpace = 0
+  !deque <- newBufferAsDeque (redundantSpace + 4 * nVerts)
+  repM_ 0 3 $ \iDir -> do
+    let !vExt = (iDir, fst start, snd start)
+    pushFront (0 :: Int, vExt) deque
+    writeIV vec vExt (0 :: Int)
+
+  let extract !w0 vExt0@(!iDir0, !y0, !x0) = do
+        !wReserved0 <- readIV vec vExt0
+        when (w0 == wReserved0) $ do
+          VU.iforM_ dirs $ \iDir (!dy, !dx) -> do
+            let !v = (y0 + dy, x0 + dx)
+            when (inRange bounds_ v && not (isBlock @! v)) $ do
+              let !w = bool (w0 + 1) w0 (iDir == iDir0)
+              let !vExt = (iDir, y0 + dy, x0 + dx)
+              !wReserved <- readIV vec vExt
+              when (wReserved == undef || w < wReserved) $ do
+                writeIV vec vExt w
+                if iDir == iDir0
+                  then pushFront (w, vExt) deque
+                  else pushBack (w, vExt) deque
+
+  -- generic BFS = pop loop
+  fix $ \loop ->
+    popFront deque >>= \case
+      Nothing -> return ()
+      Just (!w, !v) -> do
+        extract w v
+        loop
+
+  return $ vecIV vec
+  where
+    !undef = -1 :: Int
+    (!height, !width) = both succ . snd $ boundsIV isBlock
+    !bounds_ = boundsIV isBlock
+    !boundsExt = ((0, 0, 0), (3, height - 1, width - 1))
+    !nVerts = rangeSize bounds_
+    !dirs = VU.fromList [(0, 1), (0, -1), (1, 0), (-1, 0)]
