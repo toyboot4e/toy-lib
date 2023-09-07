b5b2faff9729343749eca44e295ce41c801ea066
toyboot4e <toyboot4e@gmail.com>
2023-09-07 20:59:47 +0900

    hide `modInt` template in snippet

--- template/Body.hs
+++ template/Body.hs
@@ -1,17 +1,3 @@
-data MyModulo = MyModulo
-
-instance TypeInt MyModulo where
-  -- typeInt _ = 1_000_000_007
-  typeInt _ = 998244353
-
-type MyModInt = ModInt MyModulo
-
-myModulo :: Int
-myModulo = typeInt (Proxy @MyModulo)
-
-modInt :: Int -> MyModInt
-modInt = ModInt . (`rem` myModulo)
-
 undef :: Int
 undef = -1
 
