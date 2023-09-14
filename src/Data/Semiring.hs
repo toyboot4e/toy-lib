8dcb34f8e4c64971cca8fdf605d9361e6e28d063
toyboot4e <toyboot4e@gmail.com>
2023-09-14 20:01:12 +0900

    wip `Semiring`

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -13,18 +13,24 @@ import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Base as VU
 import qualified Data.Vector.Unboxed.Mutable as VUM
 
+-- | Semiring @(s, <+>, <.>)`. Sometimes called "deoid" (double monoid).
 class Semiring s where
-  -- | Communicative monoid @(s, <+>, szero)@
+  -- | Plus (addition) opeator of the communicative monoid @(s, <+>, szero)@.
   (<+>) :: s -> s -> s
-
-  -- | Identical element of the communicative monoid @(s, <+>, szero)@
+  -- | Identical element of the communicative monoid @(s, <+>, szero)@.
   szero :: s
-
-  -- | Monoid @(s, <.>, sone)@
+  -- | Times (mulitplication) operator of the monoid @(s, <.>, sone)@.
   (<.>) :: s -> s -> s
-
   -- | Identical element of the monoid @(s, <.>, sone)@
   sone :: s
+
+-- | Fold using the `<+>` (plus) opreator.
+foldP :: (Semiring a, VG.Vector v a) => v a -> a
+foldP = VG.foldl' (<+>) szero
+
+-- | Fold using the `<.>` (times) opreator.
+foldT :: (Semiring a, VG.Vector v a) => v a -> a
+foldT = VG.foldl' (<.>) sone
 
 newtype MaxPlus a = MaxPlus {getMaxPlus :: a}
   deriving (VP.Prim)
@@ -42,15 +48,15 @@ instance (VP.Prim a) => VU.Unbox (MaxPlus a)
 
 instance (Num a, Bounded a, Ord a) => Semiring (MaxPlus a) where
   {-# INLINE (<+>) #-}
-  (MaxPlus x1) <+> (MaxPlus x2) = MaxPlus (x1 + x2)
+  (MaxPlus x1) <+> (MaxPlus x2) = MaxPlus (x1 `max` x2)
+
+  -- monoid with @<+>@ operator
+  {-# INLINE szero #-}
+  szero = MaxPlus minBound
 
   {-# INLINE (<.>) #-}
-  (MaxPlus x1) <.> (MaxPlus x2) = MaxPlus (x1 `max` x2)
+  (MaxPlus x1) <.> (MaxPlus x2) = MaxPlus (x1 + x2)
 
-  -- monoid with @+@ operator
-  {-# INLINE szero #-}
-  szero = MaxPlus 0
-
-  -- monoid with @max@ operator
+  -- monoid with @<.>@ operator
   {-# INLINE sone #-}
-  sone = MaxPlus minBound
+  sone = MaxPlus 0
