947e4cc8c8bf01e3f3c5ffbff9aa5e52ba0ddff2
toyboot4e <toyboot4e@gmail.com>
2023-09-14 20:56:49 +0900

    add `MinPlus` and `Boolean` semirings

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,9 +1,9 @@
--- | `Semiring`
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 
+-- | `Semiring`
 module Data.Semiring where
 
 import qualified Data.Vector.Generic as VG
@@ -17,10 +17,13 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 class Semiring s where
   -- | Plus (addition) opeator of the communicative monoid @(s, <+>, szero)@.
   (<+>) :: s -> s -> s
+
   -- | Identical element of the communicative monoid @(s, <+>, szero)@.
   szero :: s
+
   -- | Times (mulitplication) operator of the monoid @(s, <.>, sone)@.
   (<.>) :: s -> s -> s
+
   -- | Identical element of the monoid @(s, <.>, sone)@
   sone :: s
 
@@ -32,6 +35,7 @@ foldP = VG.foldl' (<+>) szero
 foldT :: (Semiring a, VG.Vector v a) => v a -> a
 foldT = VG.foldl' (<.>) sone
 
+-- | Max-Plus semiring
 newtype MaxPlus a = MaxPlus {getMaxPlus :: a}
   deriving (VP.Prim)
   deriving newtype (Eq, Ord, Show)
@@ -60,3 +64,73 @@ instance (Num a, Bounded a, Ord a) => Semiring (MaxPlus a) where
   -- monoid with @<.>@ operator
   {-# INLINE sone #-}
   sone = MaxPlus 0
+
+-- | Min-Plus semiring
+newtype MinPlus a = MinPlus {getMinPlus :: a}
+  deriving (VP.Prim)
+  deriving newtype (Eq, Ord, Show)
+
+newtype instance VU.MVector s (MinPlus a) = MV_MinPlus (VP.MVector s (MinPlus a))
+
+newtype instance VU.Vector (MinPlus a) = V_MinPlus (VP.Vector (MinPlus a))
+
+deriving via (VU.UnboxViaPrim (MinPlus a)) instance (VP.Prim a) => VGM.MVector VUM.MVector (MinPlus a)
+
+deriving via (VU.UnboxViaPrim (MinPlus a)) instance (VP.Prim a) => VG.Vector VU.Vector (MinPlus a)
+
+instance (VP.Prim a) => VU.Unbox (MinPlus a)
+
+instance (Num a, Bounded a, Ord a) => Semiring (MinPlus a) where
+  {-# INLINE (<+>) #-}
+  (MinPlus x1) <+> (MinPlus x2) = MinPlus (x1 `min` x2)
+
+  -- monoid with @<+>@ operator
+  {-# INLINE szero #-}
+  szero = MinPlus maxBound
+
+  {-# INLINE (<.>) #-}
+  (MinPlus x1) <.> (MinPlus x2) = MinPlus (x1 + x2)
+
+  -- monoid with @<.>@ operator
+  {-# INLINE sone #-}
+  sone = MinPlus 0
+
+-- | Boolean semiring
+newtype Boolean = Boolean {getBoolean :: Bool}
+  -- deriving (VP.Prim)
+  deriving newtype (Eq, Ord, Show)
+
+-- NOTE: For some reason, the `primitive` package does not implement `Prim` for `Bool`.
+-- I guess that's why `bitvec` exists: <https://github.com/Bodigrim/bitvec>
+--
+-- Here's some indirection to get `Unbox` implementation..
+instance VU.IsoUnbox Boolean Bool where
+  {-# INLINE toURepr #-}
+  toURepr (Boolean b) = b
+  {-# INLINE fromURepr #-}
+  fromURepr = Boolean
+
+newtype instance VU.MVector s Boolean = MV_Foo (VU.MVector s Bool)
+
+newtype instance VU.Vector Boolean = V_Foo (VU.Vector Bool)
+
+deriving via (Boolean `VU.As` Bool) instance VGM.MVector VUM.MVector Boolean
+
+deriving via (Boolean `VU.As` Bool) instance VG.Vector VU.Vector Boolean
+
+instance VU.Unbox Boolean
+
+instance Semiring Boolean where
+  {-# INLINE (<+>) #-}
+  (Boolean x1) <+> (Boolean x2) = Boolean (x1 || x2)
+
+  -- monoid with @<+>@ operator
+  {-# INLINE szero #-}
+  szero = Boolean False
+
+  {-# INLINE (<.>) #-}
+  (Boolean x1) <.> (Boolean x2) = Boolean (x1 && x2)
+
+  -- monoid with @<.>@ operator
+  {-# INLINE sone #-}
+  sone = Boolean True
