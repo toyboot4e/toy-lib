02b4424b25d2c28259e9bfaed16ec7211208362b
toyboot4e <toyboot4e@gmail.com>
2023-09-16 13:15:59 +0900

    add notes on `exgcd` and modular multicative inverse

--- src/Math/Exgcd.hs
+++ src/Math/Exgcd.hs
@@ -9,6 +9,22 @@
 module Math.Exgcd where
 
 -- | @4tsuzuru <https://zenn.dev/link/comments/29d659a57ead56>
+--
+-- @exgcd a b@ returns @(g, na, nb)@ where \(g = a \cdot n_a + b \cdot n_b\).
+--
+-- >>> exgcd 3 5
+-- (1,2,-1)
+-- >>> 1 == 3 * 2 + 5 * (-1)
+-- True
+--
+-- Note that \(n_{a} = a^{-1} \mod b\):
+-- \[
+-- \begin{aligned}
+-- n_a a + n_b b &= 1 \\
+-- \Rightarrow n_a a &\equiv 1 \mod b \\
+-- \Leftrightarrow n_a &\equiv a^{-1} \mod b
+-- \end{aligned}
+-- \]
 exgcd :: Integral a => a -> a -> (a, a, a)
 exgcd a b = f $ go a b 1 0 0 1
   where
@@ -22,3 +38,10 @@ exgcd a b = f $ go a b 1 0 0 1
     f (!g, !u, !v)
       | g < 0 = (-g, -u, -v)
       | otherwise = (g, u, v)
+
+-- | Example for showing how to use `exgcd` to calculate the modular multicative inverse.
+invModGcd :: Integral a => a -> a -> Maybe a
+invModGcd a m = case exgcd a m of
+  (1, na, _) -> Just na
+  _ -> Nothing
+
