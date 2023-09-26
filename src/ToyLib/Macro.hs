6c75db640de716f29e8a5b39f5eed80a987513bd
toyboot4e <toyboot4e@gmail.com>
2023-09-26 22:12:13 +0900

    add `dbgId` and `$$`

--- src/ToyLib/Macro.hs
+++ src/ToyLib/Macro.hs
@@ -10,6 +10,9 @@ module ToyLib.Macro where
 dbg :: Show a => a -> ()
 dbg !x = let !_ = traceShow x () in ()
 
+dbgId :: Show a => a -> a
+dbgId !x = let !_ = traceShow x () in x
+
 dbgAssert :: Bool -> String -> ()
 dbgAssert False !s = error $ "assertion failed!: " ++ s
 dbgAssert True _ = ()
@@ -18,8 +21,16 @@ dbgAssert True _ = ()
 dbg :: Show a => a -> ()
 dbg _ = ()
 
+dbgId :: Show a => a -> a
+dbgId = id
+
 dbgAssert :: Bool -> a -> a
 dbgAssert = flip const
 
 #endif
+
+($$) :: (Show a) => (a -> b) -> a -> b
+($$) lhs rhs = lhs (dbgId rhs)
+
+infixr 0 $$
 
--- template/Macros.hs
+++ template/Macros.hs
@@ -1,5 +1,6 @@
 #ifdef DEBUG
-dbg :: Show a => a -> () ; dbg !x = let !_ = traceShow x () in () ; dbgAssert :: Bool -> String -> () ; dbgAssert False !s = error $ "assertion failed!: " ++ s ; dbgAssert True _ = () ; dbgS :: String -> () ; dbgS !s = let !_ = trace s () in () ;
+dbg :: Show a => a -> () ; dbg !x = let !_ = traceShow x () in () ; dbgAssert :: Bool -> String -> () ; dbgAssert False !s = error $ "assertion failed!: " ++ s ; dbgAssert True _ = () ; dbgS :: String -> () ; dbgS !s = let !_ = trace s () in () ; dbgId :: Show a => a -> a ; dbgId !x = let !_ = traceShow x () in x ;
 #else
-dbg :: Show a => a -> () ; dbg _ = () ; dbgAssert :: Bool -> a -> a ; dbgAssert = flip const ; dbgS :: String -> () ; dbgS _ = () ;
+dbg :: Show a => a -> () ; dbg _ = () ; dbgAssert :: Bool -> a -> a ; dbgAssert = flip const ; dbgS :: String -> () ; dbgS _ = () ; dbgId :: Show a => a -> a ; dbgId = id ;
 #endif
+
