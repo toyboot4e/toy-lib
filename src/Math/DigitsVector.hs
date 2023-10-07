cfa032d76f97a10594d57be479d122e5c3b96b18
toyboot4e <toyboot4e@gmail.com>
2023-10-07 17:46:29 +0900

    add `toNDigitsVU`

--- src/Math/DigitsVector.hs
+++ src/Math/DigitsVector.hs
@@ -14,6 +14,13 @@ toDigitsVU !base !x0
     expand 0 = Nothing
     expand x = Just $ swap (x `divMod` base)
 
+-- | >>> toNDigitsVU 3 3 ((3^0)*1 + (3^1)*0 + (3^2)*0)
+-- [1,0,0]
+toNDigitsVU :: Int -> Int -> Int -> VU.Vector Int
+toNDigitsVU !base !nDigits !x0 = VU.unfoldrExactN nDigits expand x0
+  where
+    expand x = swap (x `divMod` base)
+
 -- | >>> unDigitsVU 3 (VU.fromList [1,2,1]) == ((3^0)*1 + (3^1)*2 + (3^2)*1)
 -- True
 -- >>> unDigitsVU 3 (toDigitsVU 3 ((3^0)*1 + (3^1)*2 + (3^2)*1)) == ((3^0)*1 + (3^1)*2 + (3^2)*1)
