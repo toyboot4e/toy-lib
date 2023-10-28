7edcecb3d0cb88ed0366fc73ebbe1ae168e57acc
toyboot4e <toyboot4e@gmail.com>
2023-10-28 22:47:39 +0900

    fix stack script header

--- template/Header.hs
+++ template/Header.hs
@@ -1,3 +1,3 @@
 #!/usr/bin/env stack
-{- stack script --resolver lts-21.6 --package array --package bytestring --package containers --package extra --package hashable --package unordered-containers --package heaps --package utility-ht --package vector --package vector-algorithms --package primitive --package transformers --ghc-options "-D DEBUG" -}
+{- stack script --resolver lts-21.6 --package array --package bytestring --package containers --package extra --package hashable --package unordered-containers --package heaps --package utility-ht --package vector --package vector-algorithms --package primitive --package random --package transformers --ghc-options "-D DEBUG" -}
 {-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds #-}
