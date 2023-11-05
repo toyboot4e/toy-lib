a6c6164e6eeff1aa1cb86279086a058b5c261cc1
toyboot4e <toyboot4e@gmail.com>
2023-11-05 18:22:12 +0900

    fixup header

--- template/Header.hs
+++ template/Header.hs
@@ -1,4 +1,4 @@
 #!/usr/bin/env stack
-{- stack script --resolver lts-21.6 --package array --package bytestring --package containers --package deepseq --package extra --package hashable --package unordered-containers --package heaps --package utility-ht --package vector --package vector-algorithms --package primitive --package random --package --package quickcheck transformers --ghc-options "-D DEBUG" -}
+{- stack script --resolver lts-21.6 --package array --package bytestring --package containers --package deepseq --package extra --package hashable --package unordered-containers --package heaps --package utility-ht --package vector --package vector-algorithms --package primitive --package random --package quickcheck --package transformers --ghc-options "-D DEBUG" -}
 {-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds #-}
 {-# RULES "Force inline VAI.sort" VAI.sort = VAI.sortBy compare #-}
