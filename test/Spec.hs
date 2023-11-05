2043747cdeea461994e4c94b4fa7806316379a40
toyboot4e <toyboot4e@gmail.com>
2023-11-05 21:57:37 +0900

    generate cabal script

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,16 @@
+module Main where
+
 import Test.DocTest
+
+-- Running `doctest`:
+-- $ cabal repl --with-ghc=doctest --repl-options='-w -Wdefault'
+
+-- Running `haddock`:
+-- $ cabal haddock
+-- $ cabal haddock --open
 
 -- | `stack test` where we run `doctest`.
 main :: IO ()
 main = do
-  -- Run `doctest` over all the source files:
-  doctest ["-isrc", "src/"]
+  putStrLn "TEST"
+
