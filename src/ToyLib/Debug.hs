9590d4b02361cfbb3ba5ab3c490614ef04e312e7
toyboot4e <toyboot4e@gmail.com>
2023-11-07 21:47:24 +0900

    add `dbgSTree`, `dbgSTreeAll`

--- src/ToyLib/Debug.hs
+++ src/ToyLib/Debug.hs
@@ -5,6 +5,8 @@ module ToyLib.Debug where
 import qualified Data.Vector.Generic as G
 import Data.Vector.IxVector
 import ToyLib.Macro
+import Data.SegmentTree.Strict
+import qualified Data.Vector.Generic.Mutable as GM
 
 -- | For use with `dbgS`
 class ShowGrid a where
@@ -26,4 +28,20 @@ infixr 0 $$
 g .$ f = \a -> let !b = dbgId (f a) in g b
 infixr 9 .$
 
--- TODO: Array
+dbgSTree :: (Show (v a), GM.MVector (G.Mutable v) a, G.Vector v a, PrimMonad m) => SegmentTree (G.Mutable v) (PrimState m) a -> m ()
+dbgSTree (SegmentTree _ mVec) = do
+  !vec <- G.unsafeFreeze mVec
+  -- REMARK: I'm using 0-based index and it has 2^n - 1 vertices
+  let !_ = dbg (G.drop (G.length vec `div` 2 - 1) vec)
+  return ()
+
+dbgSTreeAll :: (Show (v a), GM.MVector (G.Mutable v) a, G.Vector v a, PrimMonad m) => SegmentTree (G.Mutable v) (PrimState m) a -> m ()
+dbgSTreeAll (SegmentTree _ mVec) = do
+  !vec <- G.unsafeFreeze mVec
+  flip fix (0 :: Int, 1 :: Int) $ \loop (!n, !len) -> do
+    -- REMARK: I'm using 0-based index and it has 2^n - 1 vertices
+    unless (G.length vec <= len) do
+      let !vec' = G.take len . G.drop (len - 1) $ vec
+      let !_ = dbgS $ "> " ++ show vec'
+      loop (n + 1, 2 * len)
+
