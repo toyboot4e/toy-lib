65cf8e8d3ae43d2cedec7d00d40ab6a6bdc119fd
toyboot4e <toyboot4e@gmail.com>
2023-11-11 19:40:36 +0900

    ignore orphan rule warnings on contest

--- template/Header.hs
+++ template/Header.hs
@@ -1,3 +1,3 @@
 #!/usr/bin/env stack
-{- stack script --resolver lts-21.6 --package array --package bytestring --package containers --package deepseq --package extra --package hashable --package unordered-containers --package heaps --package utility-ht --package vector --package vector-algorithms --package primitive --package QuickCheck --package random --package transformers --ghc-options "-D DEBUG" -}
-{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds #-}
+{- stack script --resolver lts-21.6 --package array --package bytestring --package containers --package deepseq --package extra --package hashable --package unordered-containers --package heaps --package mtl --package utility-ht --package vector --package vector-algorithms --package primitive --package QuickCheck --package random --package transformers --ghc-options "-D DEBUG" -}
+{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-orphans #-}
