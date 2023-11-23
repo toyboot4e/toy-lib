e11f36bf7408105af89a25f27038ae4e838e77aa
toyboot4e <toyboot4e@gmail.com>
2023-11-23 11:50:31 +0900

    generic 01-BFS

--- src/Data/SparseGraph.hs
+++ src/Data/SparseGraph.hs
@@ -15,13 +15,14 @@ import Control.Monad.ST
 import Data.Array.IArray
 import Data.Bifunctor
 import Data.BinaryLifting
+import Data.Bool (bool)
 import Data.Buffer
 import Data.Graph (Vertex)
 import qualified Data.Heap as H
 import Data.Maybe
 import Data.Tree.Lca (LcaCache, ToParent (..))
+import Data.Tuple.Extra (both)
 import Data.Unindex
-import qualified Data.Vector.Fusion.Stream.Monadic as MS
 import qualified Data.Vector.Generic as G
 import Data.Vector.IxVector
 import qualified Data.Vector.Unboxed as U
@@ -188,13 +189,13 @@ componentsVecSG gr@SparseGraph {..} !startIx = IxVector boundsSG $ U.create $ do
     !start = index boundsSG startIx :: Vertex
 
 -- | /O(V+E)/ breadth-first search. Unreachable vertices have length of @-1@.
-bfsSG :: (HasCallStack, Ix i, U.Unbox i) => SparseGraph i w -> i -> IxVector i (U.Vector Int)
+bfsSG :: (HasCallStack, Ix i) => SparseGraph i w -> i -> IxVector i (U.Vector Int)
 bfsSG gr@SparseGraph {..} !startIx =
   IxVector boundsSG $
-    genericBFS (gr `adj`) nVertsSG nEdgesSG (index boundsSG startIx)
+    genericBfs (gr `adj`) nVertsSG nEdgesSG (index boundsSG startIx)
 
-genericBFS :: (HasCallStack) => (Int -> U.Vector Int) -> Int -> Int -> Int -> U.Vector Int
-genericBFS !gr !nVerts !nEdges !start = U.create $ do
+genericBfs :: (HasCallStack) => (Int -> U.Vector Int) -> Int -> Int -> Int -> U.Vector Int
+genericBfs !gr !nVerts !nEdges !start = U.create $ do
   let !undef = -1 :: Int
   !dist <- UM.replicate nVerts undef
   !queue <- newBufferAsQueue (nEdges + 1)
@@ -244,6 +245,84 @@ bfsGrid317E_MBuffer !isBlock !start = IxVector bounds_ $ runST $ do
     !bounds_ = boundsIV isBlock
     nexts !yx0 = U.filter ((&&) <$> inRange bounds_ <*> not . (isBlock @!)) $ U.map (add2 yx0) dyxs
     !dyxs = U.fromList [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+genericBfs01 :: (Ix i, U.Unbox i) => (i, i) -> (i -> U.Vector (i, Int)) -> U.Vector i -> IxUVector i Int
+genericBfs01 !bndExt !gr !starts = IxVector bndExt $ U.create $ do
+  let !undef = -1 :: Int
+  let !nVertsExt = rangeSize bndExt
+  !vec <- IxVector bndExt <$> UM.replicate nVertsExt undef
+  !deque <- newBufferAsDeque nVertsExt
+
+  U.forM_ starts $ \vExt -> do
+    pushFront deque (0 :: Int, vExt)
+    writeIV vec vExt (0 :: Int)
+
+  let step !w0 !vExt0 = do
+        !wReserved0 <- readIV vec vExt0
+        when (w0 == wReserved0) $ do
+          U.forM_ (gr vExt0) $ \(!vExt, !dw) -> do
+            let !w = w0 + dw
+            !wReserved <- readIV vec vExt
+            when (wReserved == undef || w < wReserved) $ do
+              writeIV vec vExt w
+              if dw == 0
+                then pushFront deque (w, vExt)
+                else pushBack deque (w, vExt)
+
+  -- generic BFS = pop loop
+  fix $ \loop ->
+    popFront deque >>= \case
+      Nothing -> return ()
+      Just (!w, !vExt) -> do
+        step w vExt
+        loop
+
+  return $ vecIV vec
+
+-- 01-BFS: zero cost with same direction.
+bfs01_grid4_typical043 :: (HasCallStack) => IxUVector (Int, Int) Bool -> (Int, Int) -> IxUVector (Int, Int, Int) Int
+bfs01_grid4_typical043 !isBlock !start = IxVector boundsExt $ U.create $ do
+  -- vec @! (dir, y, x)
+  !vec <- IxVector boundsExt <$> UM.replicate (4 * nVerts) undef
+
+  let !redundantSpace = 0
+  !deque <- newBufferAsDeque (redundantSpace + 4 * nVerts)
+  forM_ [0 .. 3] $ \iDir -> do
+    let !vExt = (iDir, fst start, snd start)
+    pushFront deque (0 :: Int, vExt)
+    writeIV vec vExt (0 :: Int)
+
+  let extract !w0 vExt0@(!iDir0, !y0, !x0) = do
+        !wReserved0 <- readIV vec vExt0
+        when (w0 == wReserved0) $ do
+          U.iforM_ dirs $ \iDir (!dy, !dx) -> do
+            let !v = (y0 + dy, x0 + dx)
+            when (inRange bounds_ v && not (isBlock @! v)) $ do
+              let !w = bool (w0 + 1) w0 (iDir == iDir0)
+              let !vExt = (iDir, y0 + dy, x0 + dx)
+              !wReserved <- readIV vec vExt
+              when (wReserved == undef || w < wReserved) $ do
+                writeIV vec vExt w
+                if iDir == iDir0
+                  then pushFront deque (w, vExt)
+                  else pushBack deque (w, vExt)
+
+  -- generic BFS = pop loop
+  fix $ \loop ->
+    popFront deque >>= \case
+      Nothing -> return ()
+      Just (!w, !v) -> do
+        extract w v
+        loop
+
+  return $ vecIV vec
+  where
+    !undef = -1 :: Int
+    (!height, !width) = both succ . snd $ boundsIV isBlock
+    !bounds_ = boundsIV isBlock
+    !boundsExt = ((0, 0, 0), (3, height - 1, width - 1))
+    !nVerts = rangeSize bounds_
+    !dirs = U.fromList [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 -- | Dijkstra: $O((E+V) \log {V})$
 --
