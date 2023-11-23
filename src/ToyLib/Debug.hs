c91f3759f65c43cdd2ff561ee064f053376ca8ce
toyboot4e <toyboot4e@gmail.com>
2023-11-23 11:51:09 +0900

    add `dbgGrid` and `dbgGridN`

--- src/ToyLib/Debug.hs
+++ src/ToyLib/Debug.hs
@@ -7,19 +7,24 @@ import Control.Monad.Primitive (PrimMonad, PrimState)
 import Data.SegmentTree.Strict
 import Data.UnionFind.Mutable
 import qualified Data.Vector.Generic as G
-import qualified Data.Vector.Generic.Mutable as GM
 import Data.Vector.IxVector
 import ToyLib.Macro
 
 -- | For use with `dbgS`
 class ShowGrid a where
   showGrid :: a -> String
+  showGridN :: Int -> a -> String
 
 instance (G.Vector v a, Show a) => ShowGrid (IxVector (Int, Int) (v a)) where
-  showGrid !grid = unlines $ map f [y0 .. y1]
+  showGrid = showGridN 0
+  showGridN !len !grid = unlines $ map f [y0 .. y1]
     where
       ((!y0, !x0), (!y1, !x1)) = boundsIV grid
-      f !y = unwords $ map (show . (grid @!) . (y,)) [x0 .. x1]
+      f !y = unwords $ map (showN . (grid @!) . (y,)) [x0 .. x1]
+      showN x =
+        let !s = show x
+            !lenX = length s
+         in replicate (len - lenX) ' ' ++ s
 
 -- | `$` with `dbgId`
 ($$) :: (Show a) => (a -> b) -> a -> b
@@ -27,11 +32,17 @@ instance (G.Vector v a, Show a) => ShowGrid (IxVector (Int, Int) (v a)) where
 
 infixr 0 $$
 
--- | `.` with `dbgId`
+-- | `.` with `dbgId`. TODO: Working?
 (.$) :: (Show b) => (b -> c) -> (a -> b) -> a -> c
 g .$ f = \a -> let !b = dbgId (f a) in g b
 
 infixr 9 .$
+
+dbgGrid :: (ShowGrid a) => a -> ()
+dbgGrid !gr = dbgS (showGrid gr)
+
+dbgGridN :: (ShowGrid a) => Int -> a -> ()
+dbgGridN !len !gr = dbgS (showGridN len gr)
 
 dbgUF :: (PrimMonad m) => MUnionFind (PrimState m) -> m ()
 dbgUF (MUnionFind vec) = dbgUM vec
@@ -42,14 +53,14 @@ dbgUM vec = do
   let !_ = dbg xs'
   return ()
 
-dbgSTree :: (Show (v a), GM.MVector (G.Mutable v) a, G.Vector v a, PrimMonad m) => SegmentTree (G.Mutable v) (PrimState m) a -> m ()
+dbgSTree :: (Show (v a), G.Vector v a, PrimMonad m) => SegmentTree (G.Mutable v) (PrimState m) a -> m ()
 dbgSTree (SegmentTree _ mVec) = do
   !vec <- G.unsafeFreeze mVec
   -- REMARK: I'm using 0-based index and it has 2^n - 1 vertices
   let !_ = dbg (G.drop (G.length vec `div` 2 - 1) vec)
   return ()
 
-dbgSTreeAll :: (Show (v a), GM.MVector (G.Mutable v) a, G.Vector v a, PrimMonad m) => SegmentTree (G.Mutable v) (PrimState m) a -> m ()
+dbgSTreeAll :: (Show (v a), G.Vector v a, PrimMonad m) => SegmentTree (G.Mutable v) (PrimState m) a -> m ()
 dbgSTreeAll (SegmentTree _ mVec) = do
   !vec <- G.unsafeFreeze mVec
   flip fix (0 :: Int, 1 :: Int) $ \loop (!n, !len) -> do
