7c21077f8b2420a77b4108cf8ef868617fa59473
toyboot4e <toyboot4e@gmail.com>
2023-12-03 09:07:42 +0900

    use `vector-algorithms` on index compression for their speed

--- src/Data/Vector/Compress.hs
+++ src/Data/Vector/Compress.hs
@@ -2,14 +2,15 @@
 module Data.Vector.Compress where
 
 import Algorithm.BinarySearch
-import Data.List.Extra (nubSort)
 import Data.Maybe
+import qualified Data.Vector.Algorithms.Intro as VAI
 import qualified Data.Vector.Unboxed as U
 import GHC.Stack (HasCallStack)
 
--- | One dimensional index compression: xs -> (indexer, xs')
+-- | One dimensional index compression: xs -> (nubSortXs, is)
 compressU :: (HasCallStack) => U.Vector Int -> (U.Vector Int, U.Vector Int)
 compressU xs = (indexer, U.map (fromJust . fst . f) xs)
   where
-    !indexer = U.fromList $ nubSort $ U.toList xs
+    -- !indexer = U.fromList $ nubSort $ U.toList xs
+    !indexer = U.uniq $ U.modify VAI.sort xs
     f !x = bsearch (0, pred $ U.length indexer) $ \i -> indexer U.! i <= x
