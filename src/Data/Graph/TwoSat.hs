ed8de233ca06747ed14aba1bd4cb5efa205f24e8
toyboot4e <toyboot4e@gmail.com>
2024-02-17 16:02:46 +0900

    add `twoSat` and `Mat2x2`

---
+++ src/Data/Graph/TwoSat.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE RecordWildCards #-}
+
+-- | 2-satisfiability problem.
+module Data.Graph.TwoSat where
+
+import Control.Monad
+import Control.Monad.Primitive (PrimMonad, PrimState)
+import Control.Monad.ST
+import Data.Array.IArray
+import Data.Bool (bool)
+import Data.Buffer
+import Data.Graph.Sparse
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import ToyLib.Macro (dbgAssert)
+
+-- | Data with True | False tag.
+data TF a = T {-# UNPACK #-} !a | F {-# UNPACK #-} !a
+
+-- | Constructs `TF` dynamically.
+asTF :: a -> Bool -> TF a
+asTF x True = T x
+asTF x False = F x
+
+-- | 2-satisfiability problem with constraints being added.
+data TwoSatBuilder s = TwoSatBuilder
+  { -- | The number of variables.
+    nVarsTSB :: !Int,
+    -- | Implication graph edges.
+    bufTSB :: !(Buffer s (Int, Int))
+  }
+
+newTSB :: (PrimMonad m) => Int -> Int -> m (TwoSatBuilder (PrimState m))
+newTSB !nVarsTSB !nMaxEdges = do
+  !bufTSB <- newBufferAsQueue nMaxEdges
+  return TwoSatBuilder {..}
+
+-- | Adds an or clause of $x1 = b1 || x2 = b2$.
+addOrClauseTSB :: (PrimMonad m) => TwoSatBuilder (PrimState m) -> TF Int -> TF Int -> m ()
+addOrClauseTSB !ts (T !x1) (T !x2) = addOrClauseTSB' ts x1 True x2 True
+addOrClauseTSB !ts (T !x1) (F !x2) = addOrClauseTSB' ts x1 True x2 False
+addOrClauseTSB !ts (F !x1) (T !x2) = addOrClauseTSB' ts x1 False x2 True
+addOrClauseTSB !ts (F !x1) (F !x2) = addOrClauseTSB' ts x1 False x2 False
+
+addOrClauseTSB' :: (PrimMonad m) => TwoSatBuilder (PrimState m) -> Int -> Bool -> Int -> Bool -> m ()
+addOrClauseTSB' TwoSatBuilder {..} x1 b1 x2 b2 = case (b1, b2) of
+  -- x1 || x2 \iff not x1 => x2, not x2 => x1
+  (True, True) -> do
+    pushBack bufTSB (x1', x2)
+    pushBack bufTSB (x2', x1)
+  -- x1 || not x2 \iff not x1 => not x2, x2 => x1
+  (True, False) -> do
+    pushBack bufTSB (x1', x2')
+    pushBack bufTSB (x2, x1)
+  -- not x1 || x2 \iff x1 => x2, not x2 => not x1
+  (False, True) -> do
+    pushBack bufTSB (x1, x2)
+    pushBack bufTSB (x2', x1')
+  -- not x1 || not x2 \iff x1 => not x2, x2 => not x1
+  (False, False) -> do
+    pushBack bufTSB (x1, x2')
+    pushBack bufTSB (x2, x1')
+  where
+    !_ = dbgAssert (inRange (0, nVarsTSB - 1) x1) $ "invalid var index: " ++ show x1
+    !_ = dbgAssert (inRange (0, nVarsTSB - 1) x2) $ "invalid var index: " ++ show x2
+    !x1' = x1 + nVarsTSB
+    !x2' = x2 + nVarsTSB
+
+solveTS :: Int -> U.Vector (Int, Int) -> Maybe (U.Vector Bool)
+solveTS !nVars !constraints = do
+  -- TODO: better SCC output
+  let gr = buildSG (0, 2 * nVars - 1) constraints
+  let !sccs = revTopSccSG gr
+  let !groups = U.create $ do
+        !vec <- UM.replicate (2 * nVars) (-1 :: Int)
+        forM_ (zip [0 :: Int ..] sccs) $ \(!iScc, !scc) -> do
+          forM_ scc $ \v -> do
+            UM.write vec v iScc
+        return vec
+
+  let !saturatable = U.all (\x -> groups U.! x /= groups U.! (x + nVars)) (U.generate nVars id)
+  if not saturatable
+    then Nothing
+    else Just $ U.map (== 1) $ U.create $ do
+      -- FIXME: better uninit representation?
+      !vec <- UM.replicate nVars (-1 :: Int)
+      forM_ sccs $ \scc -> do
+        forM_ scc $ \v -> do
+          !prev <- UM.read vec (v `mod` nVars)
+          when (prev == -1) $ do
+            -- NOTE: We're seeing from the downstream vertices!!!!
+            UM.write vec (v `mod` nVars) $ bool 1 0 (v < nVars)
+      return vec
+
+twoSat :: Int -> Int -> (forall s. TwoSatBuilder s -> ST s ()) -> Maybe (U.Vector Bool)
+twoSat !nVars !nEdges f = runST $ do
+  !tsb <- newTSB nVars nEdges
+  f tsb
+  !constraints <- unsafeFreezeBuffer (bufTSB tsb)
+  return $ solveTS nVars constraints
---
+++ src/Data/Instances/Mat2x2.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- | `Mat2x2` and `V2` as a `SemigroupAction` instance.
+module Data.Instances.Mat2x2 where
+
+import Data.Core.SemigroupAction
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as GM
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+
+-- | 2x2 unboxed matrix. Implements `SemigroupAction` to `V2`.
+newtype Mat2x2 a = Mat2x2 (Mat2x2Repr a)
+  deriving newtype (Eq, Ord, Show)
+
+-- | `Mat2x2` internal unboxed data representaton.
+type Mat2x2Repr a = (a, a, a, a)
+
+{-# INLINE unMat2x2 #-}
+unMat2x2 :: Mat2x2 a -> Mat2x2Repr a
+unMat2x2 (Mat2x2 x) = x
+
+instance (Num a) => Semigroup (Mat2x2 a) where
+  {-# INLINE (<>) #-}
+  (<>) = mulM22M22
+
+-- | Multiplies 2x2 matrix to a 2x2 matrix.
+{-# INLINE mulM22M22 #-}
+mulM22M22 :: (Num a) => Mat2x2 a -> Mat2x2 a -> Mat2x2 a
+mulM22M22 (Mat2x2 (!a11, !a12, !a21, !a22)) (Mat2x2 (!b11, !b12, !b21, !b22)) = Mat2x2 (c11, c12, c21, c22)
+  where
+    !c11 = a11 * b11 + a12 * b21
+    !c12 = a11 * b12 + a12 * b22
+    !c21 = a21 * b11 + a22 * b21
+    !c22 = a21 * b12 + a22 * b22
+
+instance (Num a) => Monoid (Mat2x2 a) where
+  -- | Identity matrix.
+  {-# INLINE mempty #-}
+  mempty = Mat2x2 (1, 0, 0, 1)
+
+instance (Num a) => SemigroupAction (Mat2x2 a) (V2 a) where
+  {-# INLINE sact #-}
+  sact = mulM22V2
+
+-- | Multiplies 2x2 matrix to a 2 vector.
+{-# INLINE mulM22V2 #-}
+mulM22V2 :: (Num a) => Mat2x2 a -> V2 a -> V2 a
+mulM22V2 (Mat2x2 (!a11, !a12, !a21, !a22)) (V2 (!x1, !x2)) = V2 (a11 * x1 + a12 * x2, a21 * x1 + a22 * x2)
+
+-- | Two-dimensional unboxed vector. Implements `Semigroup V2` as sum.
+newtype V2 a = V2 (V2Repr a) deriving newtype (Eq, Ord, Show)
+
+-- | `V2` internal unboxed data representaton.
+type V2Repr a = (a, a)
+
+{-# INLINE unV2 #-}
+unV2 :: V2 a -> V2Repr a
+unV2 (V2 x) = x
+
+instance (Num a) => Semigroup (V2 a) where
+  {-# INLINE (<>) #-}
+  (V2 (!a1, !a2)) <> (V2 (!b1, !b2)) = V2 (a1 + b1, a2 + b2)
+
+instance (Num a) => Monoid (V2 a) where
+  -- | Identity element as @Summ@.
+  {-# INLINE mempty #-}
+  mempty = V2 (0, 0)
+
+{- ORMOLU_DISABLE -}
+newtype instance U.MVector s (Mat2x2 a) = MV_Mat2x2 (U.MVector s (Mat2x2Repr a))
+newtype instance U.Vector (Mat2x2 a) = V_Mat2x2 (U.Vector (Mat2x2Repr a))
+deriving instance (U.Unbox a) => GM.MVector UM.MVector (Mat2x2 a)
+deriving instance (U.Unbox a) => G.Vector U.Vector (Mat2x2 a)
+instance (U.Unbox a) => U.Unbox (Mat2x2 a)
+
+newtype instance U.MVector s (V2 a) = MV_V2 (U.MVector s (V2Repr a))
+newtype instance U.Vector (V2 a) = V_V2 (U.Vector (V2Repr a))
+deriving instance (U.Unbox a) => GM.MVector UM.MVector (V2 a)
+deriving instance (U.Unbox a) => G.Vector U.Vector (V2 a)
+instance (U.Unbox a) => U.Unbox (V2 a)
+
+instance (Num a) => MonoidAction (Mat2x2 a) (V2 a)
+{- ORMOLU_ENABLE -}
