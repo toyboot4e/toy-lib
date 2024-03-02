b70d50f24aadee94e8284724ce459be124fa0d76
toyboot4e <toyboot4e@gmail.com>
2024-03-03 00:36:01 +0900

    inline multiset functions

--- src/Data/MultiSet.hs
+++ src/Data/MultiSet.hs
@@ -9,16 +9,20 @@ import Data.List (foldl')
 -- | Multiset: (nKeys, (key -> count))
 type MultiSet = (Int, IM.IntMap Int)
 
+{-# INLINE emptyMS #-}
 emptyMS :: MultiSet
 emptyMS = (0, IM.empty)
 
+{-# INLINE singletonMS #-}
 singletonMS :: Int -> MultiSet
 singletonMS !x = (1, IM.singleton x 1)
 
+{-# INLINE fromListMS #-}
 fromListMS :: [Int] -> MultiSet
 fromListMS = foldl' (flip incMS) emptyMS
 
 -- | Increments key.
+{-# INLINE incMS #-}
 incMS :: Int -> MultiSet -> MultiSet
 incMS !k (!n, !im) =
   if IM.member k im
@@ -26,6 +30,7 @@ incMS !k (!n, !im) =
     else (n + 1, IM.insert k 1 im)
 
 -- | Decrements key. Key with count zero are removed.
+{-# INLINE incMS #-}
 decMS :: Int -> MultiSet -> MultiSet
 decMS !k (!n, !im) =
   case IM.lookup k im of
@@ -33,18 +38,22 @@ decMS !k (!n, !im) =
     Just _ -> (n, IM.insertWith (+) k (-1) im)
     Nothing -> (n, im)
 
+{-# INLINE memberMS #-}
 memberMS :: Int -> MultiSet -> Bool
 memberMS !k (!_, !im) = IM.member k im
 
+{-# INLINE notMemberMS #-}
 notMemberMS :: Int -> MultiSet -> Bool
 notMemberMS !k (!_, !im) = IM.notMember k im
 
+{-# INLINE deleteFindMinMS #-}
 deleteFindMinMS :: MultiSet -> (Int, MultiSet)
 deleteFindMinMS ms@(!_, !im) =
   let !key = fst $ IM.findMin im
    in (key, decMS key ms)
 
 -- | Unwraps `MultiSet` into the underlying `IntMap`.
+{-# INLINE innerMS #-}
 innerMS :: MultiSet -> IM.IntMap Int
 innerMS (!_, !im) = im
 
