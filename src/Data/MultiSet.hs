f9b02ec892b03ff53032bc866218788e9b7384d4
toyboot4e <toyboot4e@gmail.com>
2024-03-05 20:03:54 +0900

    enrich `MultiSet`

--- src/Data/MultiSet.hs
+++ src/Data/MultiSet.hs
@@ -3,6 +3,7 @@ module Data.MultiSet where
 
 import qualified Data.IntMap.Strict as IM
 import Data.List (foldl')
+import Data.Maybe
 
 -- | MultiSet: (nKeys, (key -> count))
 type MultiSet = (Int, IM.IntMap Int)
@@ -22,19 +23,37 @@ fromListMS = foldl' (flip incMS) emptyMS
 -- | Increments key.
 {-# INLINE incMS #-}
 incMS :: Int -> MultiSet -> MultiSet
-incMS !k (!n, !im) =
-  if IM.member k im
-    then (n, IM.insertWith (+) k 1 im)
-    else (n + 1, IM.insert k 1 im)
+incMS !k (!nKeys, !im) = case IM.lookup k im of
+  Just !n -> (nKeys, IM.insert k (n + 1) im)
+  Nothing -> (nKeys + 1, IM.insert k 1 im)
 
--- | Decrements key. Key with count zero are removed.
-{-# INLINE incMS #-}
+-- | Decrements key.
+{-# INLINE decMS #-}
 decMS :: Int -> MultiSet -> MultiSet
-decMS !k (!n, !im) =
+decMS !k (!nKeys, !im) =
   case IM.lookup k im of
-    Just 1 -> (n - 1, IM.delete k im)
-    Just _ -> (n, IM.insertWith (+) k (-1) im)
-    Nothing -> (n, im)
+    Just 1 -> (nKeys - 1, IM.delete k im)
+    Just n -> (nKeys, IM.insert k (n - 1) im)
+    -- TODO: prefer panic?
+    Nothing -> (nKeys, im)
+
+{-# INLINE addMS #-}
+addMS :: Int -> Int -> MultiSet -> MultiSet
+addMS !k !dn (!nKeys, !im) =
+  case IM.lookup k im of
+    Just n -> (nKeys, IM.insert k (n + dn) im)
+    Nothing -> (nKeys + 1, IM.insert k dn im)
+
+{-# INLINE subMS #-}
+subMS :: Int -> Int -> MultiSet -> MultiSet
+subMS !k !dn (!nKeys, !im) =
+  case IM.lookup k im of
+    Just n
+      | n > dn -> (nKeys, IM.insert k (n - dn) im)
+      | n == dn -> (nKeys - 1, IM.delete k im)
+      -- TODO: prefer panic?
+      | n < dn -> (nKeys - 1, IM.delete k im)
+    Nothing -> (nKeys, im)
 
 {-# INLINE memberMS #-}
 memberMS :: Int -> MultiSet -> Bool
@@ -50,8 +69,16 @@ decFindMinMS ms@(!_, !im) =
   let !key = fst $ IM.findMin im
    in (key, decMS key ms)
 
+{-# INLINE lookupMS #-}
+lookupMS :: Int -> MultiSet -> Maybe Int
+lookupMS !k = IM.lookup k . innerMS
+
+-- | Partial alternative to `lookupMS`.
+{-# INLINE getMS #-}
+getMS :: Int -> MultiSet -> Int
+getMS !k = fromJust . lookupMS k
+
 -- | Unwraps `MultiSet` into the underlying `IntMap`.
 {-# INLINE innerMS #-}
 innerMS :: MultiSet -> IM.IntMap Int
 innerMS (!_, !im) = im
-
