fd14f86378b2534ffa5795736dabc7ae0a3add33
toyboot4e <toyboot4e@gmail.com>
2024-03-10 11:52:46 +0900

    inline `fromVec`

--- src/Data/Utils/FromVec.hs
+++ src/Data/Utils/FromVec.hs
@@ -21,37 +21,51 @@ class FromVec a where
 instance FromVec (IM.IntMap a) where
   type FromVecInput (IM.IntMap a) = (Int, a)
   type FromVecItem (IM.IntMap a) = a
+  {-# INLINE fromVec #-}
   fromVec = G.foldl' (\im (!k, !v) -> IM.insert k v im) IM.empty
+  {-# INLINE fromVecWith #-}
   fromVecWith !f = G.foldl' (\im (!k, !v) -> IM.insertWith f k v im) IM.empty
-
-instance (Ord k) => FromVec (M.Map k a) where
-  type FromVecInput (M.Map k a) = (k, a)
-  type FromVecItem (M.Map k a) = a
-  fromVec = G.foldl' (\im (!k, !v) -> M.insert k v im) M.empty
-  fromVecWith !f = G.foldl' (\im (!k, !v) -> M.insertWith f k v im) M.empty
 
 instance FromVec IS.IntSet where
   type FromVecInput IS.IntSet = Int
   type FromVecItem IS.IntSet = Int
+  {-# INLINE fromVec #-}
   fromVec = G.foldl' (flip IS.insert) IS.empty
+  {-# INLINE fromVecWith #-}
   fromVecWith _ = fromVec
 
-instance (Ord a) => FromVec (S.Set a) where
-  type FromVecInput (S.Set a) = a
-  type FromVecItem (S.Set a) = a
+instance (Ord k) => FromVec (M.Map k a) where
+  type FromVecInput (M.Map k a) = (k, a)
+  type FromVecItem (M.Map k a) = a
+  {-# INLINE fromVec #-}
+  fromVec = G.foldl' (\im (!k, !v) -> M.insert k v im) M.empty
+  {-# INLINE fromVecWith #-}
+  fromVecWith !f = G.foldl' (\im (!k, !v) -> M.insertWith f k v im) M.empty
+
+instance (Ord k) => FromVec (S.Set k) where
+  type FromVecInput (S.Set k) = k
+  type FromVecItem (S.Set k) = k
+  {-# INLINE fromVec #-}
   fromVec = G.foldl' (flip S.insert) S.empty
+  {-# INLINE fromVecWith #-}
+  fromVecWith _ = fromVec
+
   fromVecWith _ = fromVec
 
 instance (Ord a) => FromVec (H.Heap a) where
   type FromVecInput (H.Heap a) = a
   type FromVecItem (H.Heap a) = a
+  {-# INLINE fromVec #-}
   fromVec = G.foldl' (flip H.insert) H.empty
+  {-# INLINE fromVecWith #-}
   fromVecWith _ = fromVec
 
 instance FromVec MultiSet where
   type FromVecInput MultiSet = Int
   type FromVecItem MultiSet = Int
+  {-# INLINE fromVec #-}
   fromVec = G.foldl' (flip incMS) emptyMS
+  {-# INLINE fromVecWith #-}
   fromVecWith _ = fromVec
 
 -- | Strongly typed `fromVec`.
