557d0295709ce9cde07d138a2b3d7738ea0ce061
toyboot4e <toyboot4e@gmail.com>
2024-03-10 11:54:44 +0900

    clean

--- src/Data/Vector/Compress.hs
+++ src/Data/Vector/Compress.hs
@@ -3,21 +3,22 @@ module Data.Vector.Compress where
 
 import Algorithm.Bisect
 import Data.Maybe
+import Data.Ord (comparing)
 import qualified Data.Vector.Algorithms.Intro as VAI
-import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Unboxed as U
 import GHC.Stack (HasCallStack)
 
 -- | One dimensional index compression: xs -> (nubSortXs, xs')
 {-# INLINE compressU #-}
 compressU :: (HasCallStack) => U.Vector Int -> (U.Vector Int, U.Vector Int)
-compressU xs = (nubSortXs, U.map (bindex nubSortXs) xs)
+compressU xs = (dict, U.map (bindex dict) xs)
   where
     -- TODO: use radix sort
-    !nubSortXs = U.uniq $ U.modify VAI.sort xs
+    -- NOTE: `U.modify VAI.sort` is super slow on GHC 9.4.5
+    !dict = U.uniq $ U.modify (VAI.sortBy (comparing id)) xs
 
--- | Index with binary search (and index compression)
+-- | Binary search-based indexing.
 {-# INLINE bindex #-}
 bindex :: (HasCallStack, G.Vector v a, Ord a) => v a -> a -> Int
-bindex !vec !xref = fromJust $ bsearchL vec (<= xref)
-
+bindex !dict !xref = fromJust $ bsearchL dict (<= xref)
