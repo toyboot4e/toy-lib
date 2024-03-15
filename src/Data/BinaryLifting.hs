004b0e86109d9d63f089c6c3c2b2fc47f811a4bc
toyboot4e <toyboot4e@gmail.com>
2024-03-15 21:55:16 +0900

    clean

--- src/Data/BinaryLifting.hs
+++ src/Data/BinaryLifting.hs
@@ -146,16 +146,6 @@ instance (U.Unbox a, SemigroupAction a b) => SemigroupAction (TransiteSemigroup 
         !b' = a `sact` b
      in (i', b')
 
--- -- | Self as target
--- Overlaping instance
--- instance (U.Unbox a, Semigroup a) => SemigroupAction (TransiteSemigroup a) (Int, a) where
---   {-# INLINE sact #-}
---   sact (TransiteSemigroup _) (-1, !b) = (-1, b)
---   sact (TransiteSemigroup vec) (!i, !b) =
---     let (!i', !a) = vec U.! i
---         !b' = a <> b
---      in (i', b')
-
 -- | The identity element of `TransiteSemigroup`.
 {-# INLINE idTransiteSemigroup #-}
 idTransiteSemigroup :: (U.Unbox a) => Int -> a -> TransiteSemigroup a
@@ -166,66 +156,3 @@ instance (U.Unbox a, Semigroup a) => BinaryLifting (TransiteSemigroup a) where
   {-# INLINE cacheBL #-}
   cacheBL = cacheBLV
 
--- -- | Storage of \(s^{2^i}\).
--- newtype BinaryLifting v m = BinaryLifting (v m)
---   deriving (Show, Eq)
---
--- -- | Calculates `BinaryLifting` of the given semigroup
--- newBinLift :: (G.Vector v s, Semigroup s) => s -> BinaryLifting v s
--- newBinLift !op0 = BinaryLifting ops
---   where
---     !ops = G.iterateN (pred 63) (\ !op -> op <> op) op0
---
--- -- | Calculates `BinaryLifting` of the given semigroup
--- newBinLiftV :: (Semigroup s) => s -> BinaryLifting V.Vector s
--- newBinLiftV = newBinLift
---
--- -- | Calculates `BinaryLifting` of the given semigroup
--- newBinLiftU :: (Semigroup s, U.Unbox s) => s -> BinaryLifting U.Vector s
--- newBinLiftU = newBinLift
---
--- -- | Binarily lifted version of `stimesMonoid`.
--- -- WARNING: Usually `sactBL` is much cheaper for semigroup actions with a boxed type.
--- stimesBL :: (HasCallStack, Semigroup s, G.Vector v s) => BinaryLifting v s -> s -> Int -> s
--- stimesBL (BinaryLifting !ops) !s0 !n = U.foldl' step s0 (U.enumFromN 0 62)
---   where
---     step !m !i
---       | testBit n i = m <> ops G.! i
---       | otherwise = m
---
--- -- | Binarily lifted version of `stimesMonoid`.
--- -- WARNING: Usually `sactBL` is much cheaper for semigroup actions with a boxed type.
--- mtimesBL :: (HasCallStack, Monoid m, G.Vector v m) => BinaryLifting v m -> Int -> m
--- mtimesBL !bin !n = stimesBL bin mempty n
---
--- -- | Binarily lifted semigroup action application.
--- sactBL :: (HasCallStack, SemigroupAction s a, G.Vector v s) => BinaryLifting v s -> a -> Int -> a
--- sactBL (BinaryLifting !ops) !acc0 !nAct = U.foldl' step acc0 (rangeG 0 62)
---   where
---     step !acc !nBit
---       | testBit nAct nBit = (ops G.! nBit) `sact` acc
---       | otherwise = acc
---
--- -- | Alias of `sactBL` for monoid action.
--- mactBL :: (HasCallStack, MonoidAction m a, G.Vector v m) => BinaryLifting v m -> a -> Int -> a
--- mactBL = sactBL
---
--- -- | Old binary lifting implementation without typeclasses. TODO: Remove
--- newDoubling :: (G.Vector v a, G.Vector v Int) => a -> (a -> a) -> v a
--- newDoubling !oper0 !squareCompositeF = G.scanl' step oper0 $ G.enumFromN (1 :: Int) 62
---   where
---     step !oper !_ = squareCompositeF oper
---
--- -- | Old binary lifting implementation without typeclasses. TODO: Remove
--- newDoublingV :: a -> (a -> a) -> V.Vector a
--- newDoublingV = newDoubling
---
--- -- | Old binary lifting implementation without typeclasses. TODO: Remove
--- applyDoubling :: (HasCallStack, G.Vector v op) => v op -> a -> (a -> op -> a) -> Int -> a
--- applyDoubling !opers !x0 !act !n = foldl' step x0 [0 .. 62]
---   where
---     !_ = dbgAssert $ G.length opers == 63
---     step !acc !nBit =
---       if testBit n nBit
---         then acc `act` (opers G.! nBit)
---         else acc
