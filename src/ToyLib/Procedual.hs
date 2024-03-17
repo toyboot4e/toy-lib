a07f80941c32797d61f4bb93d268904c7f61839a
toyboot4e <toyboot4e@gmail.com>
2024-03-17 14:43:32 +0900

    proc

--- src/ToyLib/Procedual.hs
+++ src/ToyLib/Procedual.hs
@@ -1,11 +1,21 @@
 -- | The great good procedual programming.
 module ToyLib.Procedual where
 
-orM' :: (Monad m) => Bool -> m Bool -> m Bool
-orM' True _ = return True
-orM' False b = b
+or1 :: (Monad m) => Bool -> m Bool -> m Bool
+or1 True _ = return True
+or1 False b = b
 
-andM' :: (Monad m) => Bool -> m Bool -> m Bool
-andM' False _ = return False
-andM' True b = b
+and1 :: (Monad m) => Bool -> m Bool -> m Bool
+and1 False _ = return False
+and1 True b = b
+
+or2 :: (Monad m) => m Bool -> m Bool -> m Bool
+or2 x y = do
+  b <- x
+  or1 b y
+
+and2 :: (Monad m) => Bool -> m Bool -> m Bool
+and2 x y = do
+  b <- x
+  and1 b y
 
