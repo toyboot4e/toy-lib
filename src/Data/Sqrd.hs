b75d5079aa934720bd90b35151156aadbdcb671c
toyboot4e <toyboot4e@gmail.com>
2024-04-13 01:07:40 +0900

    sqrd

--- src/Data/Sqrd.hs
+++ src/Data/Sqrd.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 
--- | [Sqrt-decomposition](https://cp-algorithms.com/data_structures/sqrt_decomposition.html)
+-- | [Sqrt-decomposition](https://cp-algorithms.com/data_structures/sqrt_decomposition.html).
 module Data.Sqrd where
 
 import Control.Monad
@@ -11,9 +11,11 @@ import qualified Data.Vector.Unboxed as U
 type BlockIndex = Int
 
 -- | [Sqrt-decomposition](https://cp-algorithms.com/data_structures/sqrt_decomposition.html)
+--
+-- = Typical problems
+-- - [ABC 339 G - Smaller Sum](https://atcoder.jp/contests/abc339/tasks/abc339_g) (easy)
 data Sqrd b ret act m = Sqrd
   { blockLenSqrd :: !Int,
-    blocksSqrd :: !(VM.MVector (PrimState m) b),
     -- Fold
     readFullSqrd :: !(BlockIndex -> m ret),
     readPartSqrd :: !(BlockIndex -> Int -> Int -> m ret),
@@ -24,6 +26,7 @@ data Sqrd b ret act m = Sqrd
   }
 
 -- TODO: propagate before folding
+
 foldSqrd :: (PrimMonad m) => Sqrd b ret act m -> Int -> Int -> m ret
 foldSqrd Sqrd {..} l r = do
   let (!il, !remL) = l `divMod` blockLenSqrd
@@ -51,30 +54,26 @@ actSqrd Sqrd {..} act l r = do
       U.mapM_ (`actFullSqrd` act) $ U.generate (ir - 1 - il) (+ (il + 1))
       actPartSqrd ir act 0 remR
 
--- !readX <- newMutVar (0 :: Int)
+-- -- O(N \log N)
+-- let !xss = V.unfoldrExactN nBlocks (U.splitAt blockLenSqrd) xs
+-- !blocks <- V.forM xss $ \xs' -> do
+--   !vec <- UM.replicate (G.length dict) (0 :: Int)
+--   U.forM_ xs' $ \x -> do
+--     UM.modify vec (+ x) (bindex dict x)
+--   csum1D <$> U.unsafeFreeze vec
+--
 -- !srd <- do
---   let !xss = V.unfoldrExactN nBlocks (U.splitAt blockLenSqrd) xs
---
---   -- no need of mutability though
---   !blocksSqrd <- V.unsafeThaw <=< V.forM xss $ \xs' -> do
---     !vec <- UM.replicate (G.length dict) (0 :: Int)
---     U.forM_ xs' $ \x -> do
---       UM.modify vec (+ x) (bindex dict x)
---     csum1D <$> U.unsafeFreeze vec
---
 --   let {-# INLINE readFullSqrd #-}
 --       readFullSqrd !iBlock = do
---         !x <- readMutVar readX
---         !csum <- VM.read blocksSqrd iBlock
+--         !x <- get
+--         let !csum = blocks V.! iBlock :: U.Vector Int
 --         let !i = maybe 0 (+ 1) $ bsearchL dict (<= x)
---         -- let !_ = dbg ("full", iBlock, x, csum, i)
 --         return $ csum U.! i
 --
 --   let {-# INLINE readPartSqrd #-}
 --       readPartSqrd !iBlock !l !r = do
 --         let !xs' = xss V.! iBlock
---         !x <- readMutVar readX
---         -- let !_ = dbg ("part", iBlock, (l, r), x, xs')
+--         !x <- get
 --         return $ U.sum $ U.filter (<= x) $ U.take (r + 1 - l) $ U.drop l xs'
 --
 --   let {-# INLINE mergeSqrd #-}
@@ -86,4 +85,4 @@ actSqrd Sqrd {..} act l r = do
 --   let {-# INLINE actPartSqrd #-}
 --       actPartSqrd _ (_ :: Int) _ _ = return ()
 --
-----   return Sqrd {..}
+--   return Sqrd {..}
