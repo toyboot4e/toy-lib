7d134a4cc96ee4a336bce8bb6abbb12324d177c2
toyboot4e <toyboot4e@gmail.com>
2024-04-14 00:28:47 +0900

    add `anyMG` and `allMG` for (bad) pruning

--- src/ToyLib/Procedural.hs
+++ src/ToyLib/Procedural.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE LambdaCase #-}
+
 -- | The great good procedual programming.
 module ToyLib.Procedural where
+
+import qualified Data.Vector.Generic as G
 
 or1 :: (Monad m) => Bool -> m Bool -> m Bool
 or1 True _ = return True
@@ -18,4 +22,22 @@ and2 :: (Monad m) => m Bool -> m Bool -> m Bool
 and2 x y = do
   b <- x
   and1 b y
+
+-- | Pruning.
+anyMG :: (Monad m, G.Vector v a) => (a -> m Bool) -> v a -> m Bool
+anyMG !f = fix $ \loop acc -> case G.uncons acc of
+    Nothing -> return False
+    Just (!x, !acc') -> do
+      f x >>= \case
+        True -> return True
+        False -> loop acc'
+
+-- | Pruning.
+allMG :: (Monad m, G.Vector v a) => (a -> m Bool) -> v a -> m Bool
+allMG !f = fix $ \loop acc -> case G.uncons acc of
+    Nothing -> return True
+    Just (!x, !acc') -> do
+      f x >>= \case
+        False -> return False
+        True -> loop acc'
 
