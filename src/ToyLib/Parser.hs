b55d10877f2b98998c6cfbe3d78473524f3a43de
toyboot4e <toyboot4e@gmail.com>
2024-04-20 12:13:12 +0900

    add `getDiagMat'`

--- src/ToyLib/Parser.hs
+++ src/ToyLib/Parser.hs
@@ -94,3 +94,36 @@ getMat' !h !w = IxVector ((0, 0), (h - 1, w - 1)) <$> U.replicateM (h * w) int'
 -- | Reads next @h * w@ elements as a char-based grid.
 getGrid' :: (MonadState BS.ByteString m) => Int -> Int -> m (IxUVector (Int, Int) Char)
 getGrid' !h !w = IxVector ((0, 0), (h - 1, w - 1)) <$> U.replicateM (h * w) char'
+
+-- | Gets diagnoal matrix input.
+--
+-- = Example
+--
+-- Input:
+--
+-- @
+-- 4 8 5
+-- 6 8
+-- 3
+-- @
+--
+-- Parsed result:
+--
+-- @
+-- 0 4 8 5
+-- 4 0 6 8
+-- 8 6 0 3
+-- 5 8 3 0
+-- @
+getDiagMat' :: (PrimMonad m, MonadState BS.ByteString m) => Int -> m (IxUVector (Int, Int) Int)
+getDiagMat' !n = fmap (IxVector bnd) $ do
+  !vec <- UM.replicate (n * n) (0 :: Int)
+  U.forM_ (U.generate (n - 1) id) $ \y -> do
+    !ws <- intsU'
+    U.iforM_ ws $ \i dw -> do
+      let !x = y + i + 1
+      UM.write vec (index bnd (y, x)) dw
+      UM.write vec (index bnd (x, y)) dw
+  U.unsafeFreeze vec
+  where
+    !bnd = ((0, 0), (n - 1, n - 1))
