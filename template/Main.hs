45954e9c84fe5009a4539bc94738f0910e0b5cf3
toyboot4e <toyboot4e@gmail.com>
2024-06-05 22:06:34 +0900

    add `modifyM` to the template

--- template/Main.hs
+++ template/Main.hs
@@ -27,7 +27,7 @@ import Control.Monad.State.Class
 import Control.Monad.Trans (MonadTrans, lift)
 import Control.Monad.Trans.Cont
 import Control.Monad.Trans.Maybe
-import Control.Monad.Trans.State.Strict (State, StateT, evalState, evalStateT, execState, execStateT, runState, runStateT)
+import Control.Monad.Trans.State.Strict (State, StateT, evalState, evalStateT, execState, execStateT, runState, runStateT, modifyM)
 import Data.Bifunctor
 import Data.Bits
 import Data.Bool (bool)
