f59f682fa5fe4dd57cad612c1197499ae7d2c9f6
toyboot4e <toyboot4e@gmail.com>
2024-06-09 11:47:55 +0900

    verify rerooting

---
+++ verify/src/abc348-e.hs
@@ -0,0 +1,67 @@
+-- {{{ toy-lib: https://github.com/toyboot4e/toy-lib
+{-# OPTIONS_GHC -Wno-unused-imports -Wno-unused-top-binds -Wno-orphans #-}
+{- ORMOLU_DISABLE -}
+{-# LANGUAGE BlockArguments, CPP, DataKinds, DefaultSignatures, DerivingVia, LambdaCase, MagicHash, MultiWayIf, NumDecimals, PatternSynonyms, QuantifiedConstraints, RecordWildCards, StandaloneDeriving, StrictData, TypeFamilies, ViewPatterns #-}
+import Control.Applicative;import Control.DeepSeq;import Control.Exception (assert);import Control.Monad;import Control.Monad.Fix;import Control.Monad.IO.Class;import Control.Monad.Primitive;import Control.Monad.ST;import Control.Monad.State.Class;import Control.Monad.Trans (MonadTrans, lift);import Control.Monad.Trans.Cont;import Control.Monad.Trans.Maybe;import Control.Monad.Trans.State.Strict (State, StateT(..), evalState, evalStateT, execState, execStateT, runState, runStateT);import Data.Bifunctor;import Data.Bits;import Data.Bool (bool);import Data.Char;import Data.Coerce;import Data.Either;import Data.Foldable;import Data.Function (on);import Data.Functor;import Data.Functor.Identity;import Data.IORef;import Data.Kind;import Data.List.Extra hiding (nubOn);import Data.Maybe;import Data.Ord;import Data.Primitive.MutVar;import Data.Proxy;import Data.STRef;import Data.Semigroup;import Data.Word;import Debug.Trace;import GHC.Exts (proxy#);import GHC.Float (int2Float);import GHC.Ix (unsafeIndex);import GHC.Stack (HasCallStack);import GHC.TypeLits;import System.Exit (exitSuccess);import System.IO;import System.Random;import System.Random.Stateful;import Text.Printf;import Data.Ratio;import Data.Array.IArray;import Data.Array.IO;import Data.Array.MArray;import Data.Array.ST;import Data.Array.Unboxed (UArray);import Data.Array.Unsafe;import qualified Data.Array as A;import Data.Bit;import qualified Data.ByteString.Builder as BSB;import qualified Data.ByteString.Char8 as BS;import qualified Data.ByteString.Unsafe as BSU;import Control.Monad.Extra hiding (loop);import Data.IORef.Extra;import Data.List.Extra hiding (merge);import Data.Tuple.Extra hiding (first, second);import Numeric.Extra;import Data.Bool.HT;import qualified Data.Ix.Enum as HT;import qualified Data.List.HT as HT;import qualified Data.Vector.Fusion.Bundle as FB;import qualified Data.Vector.Generic as G;import qualified Data.Vector.Generic.Mutable as GM;import qualified Data.Vector.Primitive as P;import qualified Data.Vector.Unboxed as U;import qualified Data.Vector.Unboxed.Base as U;import qualified Data.Vector.Unboxed.Mutable as UM;import qualified Data.Vector as V;import qualified Data.Vector.Mutable as VM;import qualified Data.Vector.Fusion.Bundle.Monadic as MB;import qualified Data.Vector.Fusion.Bundle.Size as MB;import qualified Data.Vector.Fusion.Stream.Monadic as MS;import qualified Data.Vector.Algorithms.Merge as VAM;import qualified Data.Vector.Algorithms.Intro as VAI;import qualified Data.Vector.Algorithms.Search as VAS;import qualified Data.IntMap.Strict as IM;import qualified Data.Map.Strict as M;import qualified Data.IntSet as IS;import qualified Data.Set as S;import qualified Data.Sequence as Seq;import qualified Data.Heap as H;import Data.Hashable;import qualified Data.HashMap.Strict as HM;import qualified Data.HashSet as HS;import qualified Test.QuickCheck as QC
+{- ORMOLU_ENABLE -}
+
+-- {{{ toy-lib import
+
+import Data.Core.SemigroupAction
+import Data.Graph.Alias
+import Data.Graph.Sparse
+import Data.Graph.Tree.TreeSG
+import ToyLib.Debug
+import ToyLib.Parser
+import ToyLib.Prelude
+import ToyLib.ShowBSB
+
+-- }}} toy-lib import
+
+{-# RULES "Force inline VAI.sort" VAI.sort = VAI.sortBy compare #-}
+
+debug :: Bool
+debug = False
+
+-- }}}
+
+
+-- | (acc, c)
+type OpRepr = (Int, Int)
+
+instance Semigroup Op where
+  {-# INLINE (<>) #-}
+  (Op (!a1, !c1)) <> (Op (!a2, !c2)) = Op (a1 + a2, c1 + c2)
+
+instance Monoid Op where
+  {-# INLINE mempty #-}
+  mempty = Op (0, 0)
+
+instance SemigroupAction Op Acc where
+  {-# INLINE sact #-}
+  sact (Op (!a, !c)) (Acc (!a0, !c0)) = Acc (a + c + a0, c + c0)
+
+-- | (acc, c)
+type AccRepr = (Int, Int)
+
+{- ORMOLU_DISABLE -}
+newtype Acc = Acc AccRepr deriving newtype (Eq, Ord, Show) ; unAcc :: Acc -> AccRepr ; unAcc (Acc x) = x ; newtype instance U.MVector s Acc = MV_Acc (U.MVector s AccRepr) ; newtype instance U.Vector Acc = V_Acc (U.Vector AccRepr) ; deriving instance GM.MVector UM.MVector Acc ; deriving instance G.Vector U.Vector Acc ; instance U.Unbox Acc ;
+newtype Op = Op OpRepr deriving newtype (Eq, Ord, Show) ; newtype instance U.MVector s Op = MV_Op (U.MVector s OpRepr) ; newtype instance U.Vector Op = V_Op (U.Vector OpRepr) ; deriving instance GM.MVector UM.MVector Op ; deriving instance G.Vector U.Vector Op ; instance U.Unbox Op ;
+{- ORMOLU_ENABLE -}
+
+solve :: StateT BS.ByteString IO ()
+solve = do
+  !n <- int'
+  !es <- U.replicateM (n - 1) ints11'
+  !cs <- intsN' n
+
+  let !gr = buildSG n $ swapDupeU es
+  let !_ = dbg (scanTreeU gr (0 ::Int) sact (\v -> Acc (0, cs U.! v)) (Op . unAcc))
+
+  let !res = dbgId $ foldTreeAllSG gr (\v -> Acc (0, cs U.! v)) (Op . unAcc)
+  printBSB $ U.minimum $ U.map (fst . unAcc) res
+
+-- verification-helper: PROBLEM https://atcoder.jp/contests/abc348/tasks/abc348_e
+-- #rerooting
+main :: IO ()
+main = runIO solve
