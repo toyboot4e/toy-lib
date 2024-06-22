58dfbf165b136bf35284113aa1ef40c87716769d
toyboot4e <toyboot4e@gmail.com>
2024-06-22 14:06:43 +0900

    add and verify `floorSumUnsigned`

---
+++ src/Math/FloorSum.hs
@@ -0,0 +1,34 @@
+module Math.FloorSum where
+
+import Data.Bool (bool)
+
+-- | Floor sum: <https://atcoder.jp/contests/practice2/tasks/practice2_c>
+--
+-- = Constraints
+-- - n > 0
+-- - m > 0
+-- - a >= 0
+-- - b >= 0
+floorSumUnsigned :: Int -> Int -> Int -> Int -> Int
+floorSumUnsigned = inner
+  where
+    inner _ 0 _ _ = 0
+    inner n m a b = n1 + n2 + n3
+      where
+        (!qa, !ra) = a `divMod` m
+        (!qb, !rb) = b `divMod` m
+        n1 = n * qb
+        n2 = qa * (n - 1) * n `div` 2
+        (!qy, !ry) = (ra * n + rb) `divMod` m
+        n3
+          | qy < 1 = 0
+          | otherwise = inner qy ra m ry
+
+-- | Floor sum: <https://atcoder.jp/contests/practice2/tasks/practice2_c>
+floorSum :: Int -> Int -> Int -> Int -> Int
+floorSum n m a b = n1 + n2 + floorSumUnsigned n m a' b'
+  where
+    a' = bool a (a `mod` m) $ a < 0
+    n2 = bool 0 (a `div` m * (n - 1) * n `div` 2) $ b < 0
+    b' = bool b (b `mod` m) $ b < 0
+    n1 = bool 0 (n * (b `div` m)) $ a < 0
---
+++ verify/src/yosupo-math-sum-of-floor-of-linear.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE CPP #-}
+#include "./__import"
+
+-- {{{ toy-lib import
+import Data.ModInt
+import Math.NTT
+import Math.FloorSum
+import ToyLib.Debug
+import ToyLib.Parser
+import ToyLib.Prelude
+import ToyLib.ShowBSB
+
+-- }}} toy-lib import
+{-# RULES "Force inline VAI.sort" VAI.sort = VAI.sortBy compare #-}
+
+debug = False
+-- }}}
+
+solve :: StateT BS.ByteString IO ()
+solve = do
+  !t <- int'
+  !input <- U.replicateM t ints4'
+  printBSB . unlinesBSB $ U.map (\(!n, !m, !a, !b) -> floorSum n m a b) input
+
+-- verification-helper: PROBLEM https://judge.yosupo.jp/problem/sum_of_floor_of_linear
+main :: IO ()
+main = runIO solve
