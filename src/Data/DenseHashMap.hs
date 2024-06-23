5718eefaa6a764ac91b6c230232d04d3b2ec80c3
toyboot4e <toyboot4e@gmail.com>
2024-06-23 19:50:00 +0900

    add more to the dense hash map

--- src/Data/DenseHashMap.hs
+++ src/Data/DenseHashMap.hs
@@ -32,6 +32,7 @@ data DenseHashMap s a = DenseHashMap
     usedHM :: !(UM.MVector s Bool)
   }
 
+-- | \(O(N)\) Creates a hashmap.
 newHM :: (U.Unbox a, PrimMonad m) => Int -> m (DenseHashMap (PrimState m) a)
 newHM n = do
   let !k0 = 8
@@ -44,15 +45,19 @@ newHM n = do
   usedHM <- UM.replicate k False
   return DenseHashMap {..}
 
--- clearHM :: (U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> m ()
--- clearHM DenseHashMap {..} = do
---   GM.set usedHM False
---   UM.unsafeWrite restCapHM 0 maxCapHM
+-- | \(O(1)\) Returns the number of stored elements. Not tested
+sizeHM :: (PrimMonad m) => DenseHashMap (PrimState m) a -> m Int
+sizeHM DenseHashMap{..}= do
+  !rest <- UM.unsafeRead restCapHM 0
+  return $ maxCapHM - rest
 
--- sizeHM :: (U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> m ()
--- sizeHM = do
---   len <- G.
+-- | \(O(1)\) Clears the buffer. Not tested
+clearHM :: (PrimMonad m) => DenseHashMap (PrimState m) a -> m ()
+clearHM DenseHashMap {..} = do
+  GM.set usedHM False
+  UM.unsafeWrite restCapHM 0 maxCapHM
 
+-- | \(O(1)\) (Internal) Hash value calculation.
 hashHM :: DenseHashMap a s -> Int -> Int
 hashHM hm x = (x3 `xor` (x3 .>>. 31)) .&. maskHM hm
   where
@@ -63,6 +68,7 @@ hashHM hm x = (x3 `xor` (x3 .>>. 31)) .&. maskHM hm
     x2 = (x1 `xor` (x1 .>>. 30)) * 0xbf58476d1ce4e5b9
     x3 = (x2 `xor` (x2 .>>. 27)) * 0x94d049bb133111eb
 
+-- | \(O(1)\) (Internal) Hashed slot search.
 -- FIXME: capacity check
 indexHM :: (PrimMonad m) => DenseHashMap (PrimState m) a -> Int -> m Int
 indexHM hm@DenseHashMap {..} k = inner (hashHM hm k)
@@ -75,6 +81,13 @@ indexHM hm@DenseHashMap {..} k = inner (hashHM hm k)
         then -- TODO: powerset enumeration technique?
           inner $ (h + 1) .&. maskHM
         else return h
+
+memberHM :: (HasCallStack, PrimMonad m) => DenseHashMap (PrimState m) a -> Int -> m Bool
+memberHM hm@DenseHashMap {..} k = do
+  i <- indexHM hm k
+  b <- GM.read usedHM i
+  k' <- GM.read keyHM i
+  return $ b && k' == k
 
 readHM :: (HasCallStack, U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> Int -> m a
 readHM hm k = fromMaybe err <$> readMayHM hm k
@@ -110,14 +123,32 @@ exchangeHM hm@DenseHashMap {..} k v = do
       GM.write keyHM i k
       Just <$> GM.exchange valHM i v
 
--- TODO: modifyHM
-
-memberHM :: (HasCallStack, PrimMonad m) => DenseHashMap (PrimState m) a -> Int -> m Bool
-memberHM hm@DenseHashMap {..} k = do
+-- | Not tested
+modifyHM :: (HasCallStack, U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> (a -> a) -> Int -> m ()
+modifyHM hm@DenseHashMap {..} f k = do
   i <- indexHM hm k
   b <- GM.read usedHM i
-  k' <- GM.read keyHM i
-  return $ b && k' == k
+  if b
+    then GM.modify valHM f i
+    else error $ "modifyHM: not a member " ++ show k
+
+-- | \(O(1)\) Deletes a value. Not tested
+deleteHM :: (HasCallStack, U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> Int -> m (Maybe a)
+deleteHM hm@DenseHashMap {..} k = do
+  i <- indexHM hm k
+  b <- GM.read usedHM i
+  if b
+    then do
+      -- stored
+      GM.unsafeModify restCapHM (+ 1) 0
+      GM.write usedHM i False
+      Just <$> GM.read valHM i
+    else do
+      return Nothing
+
+-- | \(O(1)\) Deletes a value. Not tested
+deleteHM_ :: (HasCallStack, U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> Int -> m ()
+deleteHM_ hm k = void $ deleteHM hm k
 
 -- | \(O(N)\) Enumerates the stored key-value pairs.
 unsafeAssocsHM :: (HasCallStack, U.Unbox a, PrimMonad m) => DenseHashMap (PrimState m) a -> m (U.Vector (Int, a))
