a75f0d1f43ac263b09b487bf9e463f5ed8e717a2
toyboot4e <toyboot4e@gmail.com>
2024-07-15 00:24:15 +0900

    bulk

--- src/ToyLib/Contest/Grid.hs
+++ src/ToyLib/Contest/Grid.hs
@@ -1,9 +1,11 @@
 -- | Bulk imports and re-exports for contest.
 module ToyLib.Contest.Grid
   ( module Data.Vector.IxVector,
+    module ToyLib.Debug.Grid,
     module ToyLib.ShowBSB.Grid,
   )
 where
 
 import Data.Vector.IxVector
+import ToyLib.Debug.Grid
 import ToyLib.ShowBSB.Grid
