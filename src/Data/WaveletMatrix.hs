76aa82013ce28bcfefd92fff0d8dfa0a4b1d6756
toyboot4e <toyboot4e@gmail.com>
2024-08-08 20:19:51 +0900

    impl `accessWM`

--- src/Data/WaveletMatrix.hs
+++ src/Data/WaveletMatrix.hs
@@ -3,53 +3,44 @@
 -- | Wavelet Matrix.
 module Data.WaveletMatrix where
 
-import Control.Monad.Primitive (PrimMonad, PrimState)
-import Data.Bit
+import Control.Monad.ST (runST)
 import Data.Bifunctor (bimap)
+import Data.Bit
 import Data.Bits
 import qualified Data.Vector as V
+import qualified Data.Vector.Algorithms.Radix as VAR
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as GM
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
-import qualified Data.Vector.Algorithms.Radix as VAR
 
 -- | Wavelet matrix.
-data WaveletMatrix s = WaveletMatrix
+data WaveletMatrix = WaveletMatrix
   { -- \(\lceil \log N \rceil\).
     heightWM :: {-# UNPACK #-} !Int,
     -- The bit matrix. Each row represents (heightWM - 1 - iRow) bit's on/off.
-    bitsWM :: !(V.Vector (UM.MVector s Bit)),
+    bitsWM :: !(V.Vector (U.Vector Bit)),
     -- The number of zeros in each row in the bit matrix.
     nZerosWM :: !(U.Vector Int),
     -- The cummulative sum of each row in the bit matrix.
-    csumsWM :: !(V.Vector (UM.MVector s Int))
+    csumsWM :: !(V.Vector (U.Vector Int))
   }
-
--- | Frozen Wavelet matrix. TODO: Needed?
-data FrozenWaveletMatrix = FrozenWaveletMatrix
-  { -- \(\lceil \log N \rceil\).
-    heightFWM :: {-# UNPACK #-} !Int,
-    -- The bit matrix. Each row represents (heightWM - 1 - iRow) bit's on/off.
-    bitsFWM :: !(V.Vector (U.Vector Bit)),
-    -- The number of zeros in each row in the bit matrix.
-    nZerosFWM :: !(U.Vector Int),
-    -- The cummulative sum of each row in the bit matrix.
-    csumsFWM :: !(V.Vector (U.Vector Int))
-  } deriving (Eq, Show)
+  deriving (Eq, Show)
 
 -- | Creates a `WaveletMatrix`.
 --
 -- - @nx@: The number of different @x@.
 {-# INLINE newWM #-}
-newWM :: forall m. (PrimMonad m) => Int -> U.Vector Int -> m (WaveletMatrix (PrimState m))
-newWM nx xs = do
-  bitsWM <- toMat n <$> UM.unsafeNew (n * heightWM)
-  csumsWM <- toMat (n + 1) <$> UM.unsafeNew ((n + 1) * heightWM)
+newWM :: Int -> U.Vector Int -> WaveletMatrix
+newWM nx xs = runST $ do
+  orgBits <- UM.replicate (n * heightWM) $ Bit False
+  orgCsum <- UM.replicate ((n + 1) * heightWM) 0
+  let !bits = toMatUM n orgBits
+  let !csums = toMatUM (n + 1) orgCsum
   nZeros <- UM.unsafeNew heightWM
 
   vec <- U.thaw xs
-  V.iforM_ (V.zip bitsWM csumsWM) $ \iRow (!bits, !csum) -> do
+  V.iforM_ (V.zip bits csums) $ \iRow (!bits, !csum) -> do
     let !iBit = heightWM - 1 - iRow
     vec' <- U.unsafeFreeze vec
     -- U.imapM_ vec' -- TODO: which is faster, imapM_ vs unsafeWrite?
@@ -59,14 +50,15 @@ newWM nx xs = do
     -- csum
     GM.unsafeWrite csum 0 (0 :: Int)
     bits' <- U.unsafeFreeze bits
-    nOnes <- U.ifoldM'
-      ( \ !acc i (Bit b) -> do
-          let !acc' = acc + (fromEnum b :: Int)
-          GM.unsafeWrite csum (i + 1) acc'
-          return acc'
-      )
-      (0 :: Int)
-      bits'
+    nOnes <-
+      U.ifoldM'
+        ( \ !acc i (Bit !b) -> do
+            let !acc' = acc + (fromEnum b :: Int)
+            GM.unsafeWrite csum (i + 1) acc'
+            return acc'
+        )
+        (0 :: Int)
+        bits'
 
     UM.write nZeros iRow (n - nOnes)
 
@@ -74,10 +66,49 @@ newWM nx xs = do
     VAR.sortBy 2 2 (\_ x -> fromEnum (testBit x iBit)) vec
 
   -- FIXME: Is it SAFE to use unsafeFreeze over dropped vector?
-  !nZerosWM <- U.unsafeFreeze nZeros
+  bitsWM <- toMatU n <$> U.unsafeFreeze orgBits
+  nZerosWM <- U.unsafeFreeze nZeros
+  csumsWM <- toMatU (n + 1) <$> U.unsafeFreeze orgCsum
   return $ WaveletMatrix {..}
   where
     !n = G.length xs
     -- TODO: use bit operations
     (!heightWM, !_) = until ((>= nx) . snd) (bimap succ (* 2)) (0 :: Int, 1 :: Int)
-    toMat m = V.unfoldrExactN heightWM (UM.splitAt m)
+    toMatUM m = V.unfoldrExactN heightWM (UM.splitAt m)
+    toMatU m = V.unfoldrExactN heightWM (U.splitAt m)
+
+-- | Counts the number of 0 bits in interval [0, i].
+{-# INLINE rank0WM #-}
+rank0WM :: WaveletMatrix -> Int -> Int -> Int
+rank0WM wm iRow i = i + 1 - rank1WM wm iRow i
+
+-- | Counts the number of 1 bits in interval [0, i].
+{-# INLINE rank1WM #-}
+rank1WM :: WaveletMatrix -> Int -> Int -> Int
+rank1WM wm iRow i = popCount $ G.take (i + 1) (bitsWM wm G.! iRow)
+
+-- | Counts the number of 0 bits in interval [0, i).
+{-# INLINE rank0BV #-}
+rank0BV :: U.Vector Bit -> Int -> Int
+rank0BV bits i = i - rank1BV bits i
+
+-- | Counts the number of 1 bits in interval [0, i).
+{-# INLINE rank1BV #-}
+rank1BV :: U.Vector Bit -> Int -> Int
+rank1BV bits i = popCount $ G.take i bits
+
+-- | \(O(\log a)\) Returns @a[k]@.
+accessWM :: WaveletMatrix -> Int -> Int
+accessWM WaveletMatrix {..} i0 = res
+  where
+    (!_, !res) =
+      V.ifoldl'
+        ( \(!i, !acc) iRow !bits ->
+            let Bit !b = G.unsafeIndex bits i
+                !i' = if b then rank1BV bits i + (nZerosWM G.! iRow) else rank0BV bits i
+                !acc' = if b then setBit acc (heightWM - 1 - iRow) else acc
+             in (i', acc')
+        )
+        (i0, 0)
+        -- TODO: indexing can be faster
+        bitsWM
--- tests/Tests/WaveletMatrix.hs
+++ tests/Tests/WaveletMatrix.hs
@@ -1,13 +1,9 @@
 module Tests.WaveletMatrix where
-
-import Control.Monad.ST (runST)
 import Data.WaveletMatrix
-import Data.List qualified as L
 import Data.Vector qualified as V
 import Data.Vector.Unboxed qualified as U
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck as QC
 
 wmProps :: TestTree
 wmProps =
@@ -17,15 +13,27 @@ wmProps =
         -- The example in this article:
         -- https://miti-7.hatenablog.com/entry/2018/04/28/152259
         let xs = U.fromList [5 :: Int, 4, 5, 5, 2, 1, 5, 6, 1, 3, 5, 0]
-        let vec = V.map U.fromList $ V.fromList
+
+        let bits = V.map U.fromList $ V.fromList
              [ [1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0],
                [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
                [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0]
              ]
-        let vec' = runST $ do
-              wm <- newWM 6 xs
-              V.mapM U.unsafeFreeze (bitsWM wm)
-        vec @?= vec'
+
+        let ints = V.map U.fromList $ V.fromList
+             [ [1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0],
+               [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+               [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0]
+             ]
+
+        let wm = newWM 6 xs
+        bits @?= bitsWM wm
+
+        let nZeros = U.convert $ V.map ((U.length xs -) . U.sum) ints
+        nZeros @?= nZerosWM wm
+
+        let xs' = U.generate (U.length xs) (accessWM wm)
+        xs @?= xs'
     ]
 
 tests :: [TestTree]
