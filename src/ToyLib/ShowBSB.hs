e845236688d38a6cdfdf812ec4e3c3bd25cdc714
toyboot4e <toyboot4e@gmail.com>
2024-08-18 01:24:38 +0900

    inline aggressively, though makes no sense

--- src/ToyLib/ShowBSB.hs
+++ src/ToyLib/ShowBSB.hs
@@ -5,8 +5,6 @@
 -- It's good for both performance and monad transformers.
 module ToyLib.ShowBSB where
 
--- TODO: foldMap vs foldMap'
-
 import Control.Monad.IO.Class
 import Data.Bool (bool)
 import qualified Data.ByteString.Builder as BSB
@@ -34,85 +32,111 @@ wsBSB = BSB.char7 ' '
 -- | Show as a bytestring builder.
 class ShowBSB a where
   showBSB :: a -> BSB.Builder
+  {-# INLINE showBSB #-}
   default showBSB :: (Show a) => a -> BSB.Builder
   showBSB = BSB.string8 . show
 
 -- TODO: deriving?
 
 instance ShowBSB Int where
+  {-# INLINE showBSB #-}
   showBSB = BSB.intDec
 
 instance ShowBSB Integer where
+  {-# INLINE showBSB #-}
   showBSB = BSB.integerDec
 
 instance ShowBSB Float where
+  {-# INLINE showBSB #-}
   showBSB = BSB.floatDec
 
 instance ShowBSB Double where
+  {-# INLINE showBSB #-}
   showBSB = BSB.doubleDec
 
 instance ShowBSB Char where
+  {-# INLINE showBSB #-}
   showBSB = BSB.char7
 
 instance ShowBSB String where
+  {-# INLINE showBSB #-}
   -- TODO: string7 vs string8
   showBSB = BSB.string8
 
 instance ShowBSB BS.ByteString where
+  {-# INLINE showBSB #-}
   showBSB = BSB.byteString
 
 instance ShowBSB BSB.Builder where
+  {-# INLINE showBSB #-}
   showBSB = id
 
 instance (ShowBSB a, ShowBSB b) => ShowBSB (a, b) where
+  {-# INLINE showBSB #-}
   showBSB (!a, !b) = showBSB a <> BSB.string7 " " <> showBSB b
 
 instance (ShowBSB a, ShowBSB b, ShowBSB c) => ShowBSB (a, b, c) where
+  {-# INLINE showBSB #-}
   showBSB (!a, !b, !c) = showBSB a <> BSB.string7 " " <> showBSB b <> BSB.string7 " " <> showBSB c
 
 instance (ShowBSB a, ShowBSB b, ShowBSB c, ShowBSB d) => ShowBSB (a, b, c, d) where
+  {-# INLINE showBSB #-}
   showBSB (!a, !b, !c, !d) = showBSB a <> BSB.string7 " " <> showBSB b <> BSB.string7 " " <> showBSB c <> BSB.string7 " " <> showBSB d
 
+{-# INLINE showLnBSB #-}
 showLnBSB :: (ShowBSB a) => a -> BSB.Builder
 showLnBSB = (<> endlBSB) . showBSB
 
+{-# INLINE printBSB #-}
 printBSB :: (ShowBSB a, MonadIO m) => a -> m ()
 printBSB = putBSB . showLnBSB
 
+{-# INLINE concatBSB #-}
 concatBSB :: (G.Vector v a, ShowBSB a) => v a -> BSB.Builder
 concatBSB = G.foldMap showBSB
 
+{-# INLINE intersperseBSB #-}
 intersperseBSB :: (G.Vector v a, ShowBSB a) => BSB.Builder -> v a -> BSB.Builder
 intersperseBSB = intersperseWithBSB showBSB
 
+{-# INLINE intersperseWithBSB #-}
 intersperseWithBSB :: (G.Vector v a) => (a -> BSB.Builder) -> BSB.Builder -> v a -> BSB.Builder
 intersperseWithBSB showF del vec
   | G.null vec = mempty
   | otherwise = showF (G.head vec) <> G.foldMap ((del <>) . showF) (G.tail vec)
 
+{-# INLINE unwordsBSB #-}
 unwordsBSB :: (ShowBSB a, G.Vector v a) => v a -> BSB.Builder
 unwordsBSB = intersperseBSB wsBSB
 
+{-# INLINE unlinesBSB #-}
 unlinesBSB :: (ShowBSB a, G.Vector v a) => v a -> BSB.Builder
 unlinesBSB = intersperseBSB endlBSB
 
+{-# INLINE yn #-}
 yn :: Bool -> String
 yn = bool "No" "Yes"
 
+{-# INLINE ynBSB #-}
 ynBSB :: Bool -> BSB.Builder
 ynBSB = bool (BSB.string8 "No") (BSB.string8 "Yes")
 
+{-# INLINE printYn #-}
 printYn :: (MonadIO m) => Bool -> m ()
 printYn = putLnBSB . ynBSB
 
+{-# INLINE printList #-}
 printList :: (ShowBSB a, U.Unbox a, MonadIO m) => [a] -> m ()
 printList = putLnBSB . unwordsBSB . U.fromList
 
+{-# INLINE putList #-}
 putList :: (ShowBSB a, U.Unbox a, MonadIO m) => [a] -> m ()
 putList = putBSB . unwordsBSB . U.fromList
 
+{-# INLINE printVec #-}
 printVec :: (ShowBSB a, G.Vector v a, MonadIO m) => v a -> m ()
 printVec = putLnBSB . unwordsBSB
 
+{-# INLINE putVec #-}
 putVec :: (ShowBSB a, G.Vector v a, MonadIO m) => v a -> m ()
 putVec = putBSB . unwordsBSB
