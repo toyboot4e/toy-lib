e116e0b96574689e64138a2f39d90add94fd5d71
toyboot4e <toyboot4e@gmail.com>
2024-09-20 22:55:53 +0900

    fix the `SlideMin` module

--- src/Algorithm/SlideMin.hs
+++ src/Algorithm/SlideMin.hs
@@ -1,58 +1,68 @@
--- | Sliding minimum.
+-- | Sliding minimum window.
 module Algorithm.SlideMin where
 
-import Control.Monad
 import Control.Monad.Extra (whenM)
 import Control.Monad.Fix
 import Control.Monad.ST
 import Data.Buffer
-import Data.Maybe
 import Data.Ord (Down(..))
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as U
 
 -- | \(O(N)\) <https://qiita.com/kuuso1/items/318d42cd089a49eeb332>
 --
--- Returns indices of elements of the minimum values in corresponding spans.
--- Drop the first @len - 1@ elements if you don't need spans shorter than the @len@.
-{-# INLINE slideMinIndicesOn #-}
-slideMinIndicesOn :: (G.Vector v a, Ord b) => (a -> b) -> Int -> v a -> U.Vector Int
-slideMinIndicesOn wrap len xs = runST $ do
+-- TODO: Explain what it's like.
+--
+-- Returns indices of elements of the minimum values in the windows with the specified length.
+{-# INLINE slideMaxIndicesOn #-}
+slideMaxIndicesOn :: (G.Vector v a, Ord b) => (a -> b) -> Int -> v a -> U.Vector Int
+slideMaxIndicesOn wrap len xs = runST $ do
   -- queue of minimum numbers
   !buf <- newBuffer (G.length xs)
 
-  G.generateM (G.length xs) $ \i -> do
+  fmap (U.drop (len - 1)) $ G.generateM (G.length xs) $ \i -> do
     -- remove indices that are no longer in the span
     fix $ \loop -> do
       whenM (maybe False (<= i - len) <$> readMaybeFront buf 0) $ do
-        void $ popFront buf
+        popFront_ buf
         loop
 
-    -- remove indices that are less attractive to the new combing value
+    -- remove indices that are less attractive to the new coming value
     fix $ \loop -> do
       whenM (maybe False ((< wrap (xs G.! i)) . wrap . (xs G.!)) <$> readMaybeBack buf 0) $ do
-        void $ popBack buf
+        popBack_ buf
         loop
 
     pushBack buf i
-    fromJust <$> readMaybeFront buf 0
+    readFront buf 0
 
 -- | \(O(N)\)
 --
+-- = Example
+--
+-- @
+-- indices: 0 1 2 3 4 5
+-- values:  5 4 3 2 1 0   min value indices:
+--          [---]         2
+--            [---]       3
+--              [---]     4
+--                [---]   5
+-- @
+--
 -- >>> slideMinIndices 3 (U.fromList [0 .. 5])
--- [0,1,2,3,4,5]
+-- [0,1,2,3]
 -- >>> slideMinIndices 3 (U.fromList [5, 4 .. 0])
--- [0,0,0,1,2,3]
+-- [2,3,4,5]
 {-# INLINE slideMinIndices #-}
 slideMinIndices :: Int -> U.Vector Int -> U.Vector Int
-slideMinIndices = slideMinIndicesOn id
+slideMinIndices = slideMaxIndicesOn Down
 
 -- | \(O(N)\)
 --
 -- >>> slideMaxIndices 3 (U.fromList [0 .. 5])
--- [0,0,0,1,2,3]
+-- [2,3,4,5]
 -- >>> slideMaxIndices 3 (U.fromList [5, 4 .. 0])
--- [0,1,2,3,4,5]
+-- [0,1,2,3]
 {-# INLINE slideMaxIndices #-}
 slideMaxIndices :: Int -> U.Vector Int -> U.Vector Int
-slideMaxIndices = slideMinIndicesOn Down
+slideMaxIndices = slideMaxIndicesOn id
