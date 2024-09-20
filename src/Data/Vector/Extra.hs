29fe7fe0cdc2dc026730bc5e4e061de36bec2c99
toyboot4e <toyboot4e@gmail.com>
2024-09-20 21:37:01 +0900

    add `Boxed` newtype as an `Unbox` instance over boxed types

--- src/Data/Vector/Extra.hs
+++ src/Data/Vector/Extra.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Data.Vector.Extra where
 
 import Algorithm.Bisect
@@ -9,6 +11,7 @@ import qualified Data.Vector.Algorithms.Intro as VAI
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as GM
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import GHC.Stack (HasCallStack)
 
 -- | \(O(N \log N)\) One dimensional index compression: xs -> (nubSortXs, xs')
@@ -71,3 +74,53 @@ constructrMN !n f = do
         v'' <- G.unsafeFreeze v'
         fill v'' (i + 1)
     fill v _ = return v
+
+-- | Wraps a boxed type as an `Unbox` instance, dispatching the boxed vector as the backing array
+-- instance. <https://github.com/haskell/vector/issues/503>
+newtype Boxed a = Boxed a
+
+newtype instance U.MVector s (Boxed a) = MV_Boxed (V.MVector s a)
+
+newtype instance U.Vector (Boxed a) = V_Boxed (V.Vector a)
+
+instance GM.MVector UM.MVector (Boxed a) where
+  {-# INLINE basicLength #-}
+  basicLength = GM.basicLength
+  {-# INLINE basicUnsafeSlice #-}
+  basicUnsafeSlice = GM.basicUnsafeSlice
+  {-# INLINE basicOverlaps #-}
+  basicOverlaps = GM.basicOverlaps
+  {-# INLINE basicUnsafeNew #-}
+  basicUnsafeNew = GM.basicUnsafeNew
+  {-# INLINE basicInitialize #-}
+  basicInitialize = GM.basicInitialize
+  {-# INLINE basicUnsafeRead #-}
+  basicUnsafeRead = GM.basicUnsafeRead
+  {-# INLINE basicUnsafeWrite #-}
+  basicUnsafeWrite = GM.basicUnsafeWrite
+  {-# INLINE basicClear #-}
+  basicClear = GM.basicClear
+  {-# INLINE basicUnsafeCopy #-}
+  basicUnsafeCopy = GM.basicUnsafeCopy
+  {-# INLINE basicUnsafeMove #-}
+  basicUnsafeMove = GM.basicUnsafeMove
+  {-# INLINE basicUnsafeGrow #-}
+  basicUnsafeGrow = GM.basicUnsafeGrow
+
+instance G.Vector U.Vector (Boxed a) where
+  {-# INLINE basicUnsafeFreeze #-}
+  basicUnsafeFreeze = G.basicUnsafeFreeze
+  {-# INLINE basicUnsafeThaw #-}
+  basicUnsafeThaw = G.basicUnsafeThaw
+  {-# INLINE basicLength #-}
+  basicLength = G.basicLength
+  {-# INLINE basicUnsafeSlice #-}
+  basicUnsafeSlice = G.basicUnsafeSlice
+  {-# INLINE basicUnsafeIndexM #-}
+  basicUnsafeIndexM = G.basicUnsafeIndexM
+  {-# INLINE basicUnsafeCopy #-}
+  basicUnsafeCopy = G.basicUnsafeCopy
+  {-# INLINE elemseq #-}
+  elemseq = G.elemseq
+
+instance U.Unbox (Boxed a)
