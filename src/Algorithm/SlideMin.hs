b614a83e91dbbeeaa0c2c3e7af3924cf2cc9494b
toyboot4e <toyboot4e@gmail.com>
2024-09-22 13:47:45 +0900

    add `lookBackHigherIndices`

--- src/Algorithm/SlideMin.hs
+++ src/Algorithm/SlideMin.hs
@@ -5,29 +5,15 @@ import Control.Monad.Extra (whenM)
 import Control.Monad.Fix
 import Control.Monad.ST
 import Data.Buffer
+import Data.Maybe
 import Data.Ord (Down(..))
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as U
 
--- | \(O(N)\) <https://qiita.com/kuuso1/items/318d42cd089a49eeb332>
---
--- TODO: Explain what it's like.
---
--- Returns indices of maximum values in the windows with the specified length.
---
--- = Example
---
--- @
--- indices: 0 1 2 3 4 5
--- values:  0 1 2 3 4 5   max value indices:
---          [---]         2
---            [---]       3
---              [---]     4
---                [---]   5
--- @
-{-# INLINE slideMaxIndicesOn #-}
-slideMaxIndicesOn :: (G.Vector v a, Ord b) => (a -> b) -> Int -> v a -> U.Vector Int
-slideMaxIndicesOn wrap len xs = runST $ do
+-- | \(O(N)\) (1) in <https://qiita.com/kuuso1/items/318d42cd089a49eeb332>
+{-# INLINE slideCmpIndicesOn #-}
+slideCmpIndicesOn :: (G.Vector v a, Ord b) => (a -> b) -> Int -> v a -> U.Vector Int
+slideCmpIndicesOn wrap len xs = runST $ do
   -- dequeue of maximum number indices.
   !buf <- newBuffer (G.length xs)
 
@@ -47,7 +33,7 @@ slideMaxIndicesOn wrap len xs = runST $ do
     pushBack buf i
     readFront buf 0
 
--- | \(O(N)\)
+-- | \(O(N)\) Returns indices of minimum values in the windows with the specified length.
 --
 -- >>> slideMinIndices 3 (U.fromList [0 .. 5])
 -- [0,1,2,3]
@@ -55,14 +41,69 @@ slideMaxIndicesOn wrap len xs = runST $ do
 -- [2,3,4,5]
 {-# INLINE slideMinIndices #-}
 slideMinIndices :: Int -> U.Vector Int -> U.Vector Int
-slideMinIndices = slideMaxIndicesOn Down
+slideMinIndices = slideCmpIndicesOn Down
 
--- | \(O(N)\)
+-- | \(O(N)\) Returns indices of maximum values in the windows with the specified length.
+--
+-- = Example
+--
+-- @
+-- indices: 0 1 2 3 4 5
+-- values:  0 1 2 3 4 5   max value indices:
+--          [---]         2
+--            [---]       3
+--              [---]     4
+--                [---]   5
 --
 -- >>> slideMaxIndices 3 (U.fromList [0 .. 5])
 -- [2,3,4,5]
 -- >>> slideMaxIndices 3 (U.fromList [5, 4 .. 0])
 -- [0,1,2,3]
+-- @
 {-# INLINE slideMaxIndices #-}
 slideMaxIndices :: Int -> U.Vector Int -> U.Vector Int
-slideMaxIndices = slideMaxIndicesOn id
+slideMaxIndices = slideCmpIndicesOn id
+
+-- | \(O(N)\) (2) <https://qiita.com/kuuso1/items/318d42cd089a49eeb332>
+{-# INLINE lookBackIndicesOn #-}
+lookBackIndicesOn :: (G.Vector v a, Ord b) => (a -> b) -> v a -> U.Vector Int
+lookBackIndicesOn wrap xs = runST $ do
+  -- dequeue of maximum number indices. actually a stack is enough though.
+  !buf <- newBuffer (G.length xs)
+
+  G.generateM (G.length xs) $ \i -> do
+    -- remove the last indices that are less than or equal to  attractive to the new coming value:
+    fix $ \loop -> do
+      whenM (maybe False ((< wrap (xs G.! i)) . wrap . (xs G.!)) <$> readMaybeBack buf 0) $ do
+        popBack_ buf
+        loop
+
+    res <- fromMaybe (-1) <$> readMaybeBack buf 0
+    pushBack buf i
+    return res
+
+-- | \(O(N)\) Solution to the histogram problem. Find the nearest lower building for each @i@..
+{-# INLINE lookBackLowerIndices #-}
+lookBackLowerIndices :: U.Vector Int -> U.Vector Int
+lookBackLowerIndices = lookBackIndicesOn Down
+
+-- | \(O(N)\) Solution to the histogram problem. Find the nearest higher building for each @i@..
+--
+-- = Example
+--
+-- @
+-- index:  -1  0   1   2   3   4
+-- height: --  1   5   2   4   3
+--           <---- |
+--                 | <---- |
+--                 |       | <-|
+--                 | <-|   |   |
+--           <-|   |   |   |
+-- look back: -1  -1   1   1   3
+-- @
+--
+-- = Typical problems
+-- - [ABC 372 D  - Buildings](https://atcoder.jp/contests/abc372/tasks/abc372_d)
+{-# INLINE lookBackHigherIndices #-}
+lookBackHigherIndices :: U.Vector Int -> U.Vector Int
+lookBackHigherIndices = lookBackIndicesOn id
