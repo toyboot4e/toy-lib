d637b36bc27b796f0b545fbab0d909a342ab4f94
toyboot4e <toyboot4e@gmail.com>
2024-09-27 22:30:43 +0900

    verify

---
+++ verify/src/abc372-d.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE CPP #-}
+#include "./__import"
+
+-- {{{ toy-lib import
+import ToyLib.Contest.Prelude
+import Algorithm.SlideMin
+import Data.SegmentTree.Strict
+
+-- }}} toy-lib import
+{-# RULES "Force inline VAI.sort" VAI.sort = VAI.sortBy compare #-}
+#ifdef DEBUG
+debug :: Bool ; debug = True
+#else
+debug :: Bool ; debug = False
+#endif
+{- ORMOLU_ENABLE -}
+
+solve :: StateT BS.ByteString IO ()
+solve = do
+  !n <- int'
+  !hs <- intsU'
+
+  let !imos = U.create $ do
+         vec <- UM.replicate n (0 :: Int)
+         let !ls = lookBackHigherIndices hs
+         U.iforM_ ls $ \r l_ -> do
+           let !l = max 0 l_
+           GM.modify vec (+ 1) l
+           GM.modify vec (subtract 1) r
+         return vec
+
+  printVec $ U.scanl1' (+) imos
+
+-- verification-helper: PROBLEM https://atcoder.jp/contests/abc372/tasks/abc372_d
+main :: IO ()
+main = runIO solve
