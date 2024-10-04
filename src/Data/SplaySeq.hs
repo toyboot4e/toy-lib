63e3640e076e443ab976d90e4f5f17c1e10d93d2
toyboot4e <toyboot4e@gmail.com>
2024-10-05 02:32:33 +0900

    Revert "try `MutablePrimArray`. is it worth it?"
    This reverts commit 4c8929c478c03eb039b81ac202561e007b940e50.

--- src/Data/SplaySeq.hs
+++ src/Data/SplaySeq.hs
@@ -10,9 +10,10 @@ module Data.SplaySeq where
 import Control.Monad.Primitive (PrimMonad, PrimState)
 import Data.Core.SegmentAction
 import Data.Pool (PoolIndex(..))
-import Data.Primitive
 import Data.SplaySeq.Raw
+import qualified Data.Vector.Generic.Mutable as GM
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 import GHC.Stack (HasCallStack)
 
 -- | Mutable, splay tree-based sequences.
@@ -22,7 +23,7 @@ data SplaySeq s v a = SplaySeq
   { -- | The maximum number of elements.
     rawSS :: !(RawSplaySeq s v a),
     -- | The root vertex.
-    rootSS :: !(MutablePrimArray s SplayIndex)
+    rootSS :: !(UM.MVector s SplayIndex)
   }
 
 -- | \(O(N)\) Allocates a `SplaySeq` with lazily propagated values.
@@ -30,8 +31,7 @@ data SplaySeq s v a = SplaySeq
 newLazySS :: (U.Unbox v, U.Unbox a, PrimMonad m) => Int -> m (SplaySeq (PrimState m) v a)
 newLazySS n = do
   rawSS <- newRSS n
-  rootSS <- newPrimArray 1
-  writePrimArray rootSS 0 $ PoolIndex (-1)
+  rootSS <- UM.replicate 1 $ PoolIndex (-1)
   return SplaySeq {..}
 
 -- | \(O(N)\) Allocates a `SplaySeq` without lazily propagated values. It still allocates needless
@@ -54,7 +54,7 @@ sizeSS = sizeRSS . rawSS
 {-# INLINE seqSizeSS #-}
 seqSizeSS :: (HasCallStack, PrimMonad m) => SplaySeq (PrimState m) v a -> m Int
 seqSizeSS SplaySeq {..} = do
-  root <- readPrimArray rootSS 0
+  root <- GM.unsafeRead rootSS 0
   seqSizeRSS rawSS root
 
 -- * Allocation
@@ -63,14 +63,18 @@ seqSizeSS SplaySeq {..} = do
 allocSeqSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a) => SplaySeq (PrimState m) v a -> U.Vector v -> m ()
 allocSeqSS SplaySeq {..} xs = do
   root <- allocSeqRSS rawSS xs
-  writePrimArray rootSS 0 root
+  GM.unsafeWrite rootSS 0 root
 
 -- | \(O(N)\) Frees itself.
 freeSS :: (HasCallStack, PrimMonad m) => SplaySeq (PrimState m) v a -> m ()
-freeSS SplaySeq {..} = do
-  root <- readPrimArray rootSS 0
-  freeSubtreeRSS rawSS root
-  writePrimArray rootSS 0 undefSI
+freeSS SplaySeq {..} =
+  GM.unsafeModifyM
+    rootSS
+    ( \root -> do
+        freeSubtreeRSS rawSS root
+        return undefSI
+    )
+    0
 
 -- * API
 
@@ -78,91 +82,79 @@ freeSS SplaySeq {..} = do
 {-# INLINE readSS #-}
 readSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> m v
 readSS SplaySeq {..} k = do
-  root <- readPrimArray rootSS 0
+  root <- GM.unsafeRead rootSS 0
   (!x, !root') <- readRSS rawSS k root
-  writePrimArray rootSS 0 root'
+  GM.unsafeWrite rootSS 0 root'
   return x
 
 -- | Amortized \(O(\log N)\). Writes a kth node's value.
 {-# INLINE writeSS #-}
 writeSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> v -> m ()
 writeSS SplaySeq {..} k v = do
-  root <- readPrimArray rootSS 0
-  root' <- writeRSS rawSS k v root
-  writePrimArray rootSS 0 root'
+  GM.unsafeModifyM rootSS (writeRSS rawSS k v) 0
 
 -- | Amortized \(O(\log N)\). Modifies a kth node's value.
 {-# INLINE modifySS #-}
 modifySS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> (v -> v) -> Int -> m ()
 modifySS SplaySeq {..} f k = do
-  root <- readPrimArray rootSS 0
-  root' <- modifyRSS rawSS f k root
-  writePrimArray rootSS 0 root'
+  GM.unsafeModifyM rootSS (modifyRSS rawSS f k) 0
 
 -- | Amortized \(O(\log N)\). Exchanges a kth node's value.
 {-# INLINE exchangeSS #-}
 exchangeSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> v -> m v
 exchangeSS SplaySeq {..} k v = do
-  root <- readPrimArray rootSS 0
+  root <- GM.unsafeRead rootSS 0
   (!x, !root') <- exchangeRSS rawSS k v root
-  writePrimArray rootSS 0 root'
+  GM.unsafeWrite rootSS 0 root'
   return x
 
 -- | Amortized \(O(\log N)\). Folds an interval @[l, r]@.
 {-# INLINE foldSS #-}
 foldSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> Int -> m v
 foldSS SplaySeq {..} l r = do
-  root <- readPrimArray rootSS 0
+  root <- GM.unsafeRead rootSS 0
   (!x, !root') <- foldRSS rawSS l r root
-  writePrimArray rootSS 0 root'
+  GM.unsafeWrite rootSS 0 root'
   return x
 
 -- | Amortized \(O(\log N)\). Folds the whole sequence.
 {-# INLINE foldAllSS #-}
 foldAllSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> m v
 foldAllSS SplaySeq {..} = do
-  root <- readPrimArray rootSS 0
+  root <- GM.unsafeRead rootSS 0
   foldAllRSS rawSS root
 
 -- | Amortized \(O(\log N)\).
 {-# INLINE sactSS #-}
 sactSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> Int -> a -> m ()
 sactSS SplaySeq {..} l r act = do
-  root <- readPrimArray rootSS 0
-  root' <- sactRSS rawSS l r act root
-  writePrimArray rootSS 0 root'
+  GM.unsafeModifyM rootSS (sactRSS rawSS l r act) 0
 
 -- | Amortised \(O(\log N)\). Reverses the order of nodes in given range @[l, r]@. Requires the
 -- monoid and the action to be commutative.
 {-# INLINE reverseSS #-}
 reverseSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> Int -> m ()
 reverseSS SplaySeq {..} l r = do
-  root <- readPrimArray rootSS 0
-  root' <- reverseRSS rawSS l r root
-  writePrimArray rootSS 0 root'
+  GM.unsafeModifyM rootSS (reverseRSS rawSS l r) 0
 
 -- | Amortized \(O(\log N)\). Inserts a node at @k@.
 {-# INLINE insertSS #-}
 insertSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> v -> m ()
 insertSS SplaySeq {..} k v = do
-  root <- readPrimArray rootSS 0
-  root' <- insertRSS rawSS k v root
-  writePrimArray rootSS 0 root'
+  GM.unsafeModifyM rootSS (insertRSS rawSS k v) 0
 
 -- | Amortized \(O(\log N)\). Deletes a node at @k@.
 {-# INLINE deleteSS #-}
 deleteSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> Int -> m ()
 deleteSS SplaySeq {..} i = do
-  root <- readPrimArray rootSS 0
-  root' <- deleteRSS rawSS i root
-  writePrimArray rootSS 0 root'
+  GM.unsafeModifyM rootSS (deleteRSS rawSS i) 0
 
 -- | Amortized \(O(\log N)\). Bisection method over the sequence. Partition point. Note that The
 -- user function is run over each node, not fold of an interval.
 {-# INLINE bisectLSS #-}
 bisectLSS :: (HasCallStack, PrimMonad m, Monoid v, U.Unbox v, Monoid a, U.Unbox a, SegmentAction a v, Eq a) => SplaySeq (PrimState m) v a -> (v -> Bool) -> m (Maybe SplayIndex)
 bisectLSS SplaySeq {..} check = do
-  root <- readPrimArray rootSS 0
+  root <- GM.unsafeRead rootSS 0
   (!res, !root') <- bisectLRSS rawSS check root
-  writePrimArray rootSS 0 root'
+  GM.unsafeWrite rootSS 0 root'
   return res
